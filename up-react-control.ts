194911603c381c6bb721fd6711fe7dca2417dc9c
Vincent Bouzon <vincent.bouzon@live.fr>
2017-04-07 17:49:55 +0200

    fix path

--- up-react-control.ts
+++ up-react-control.ts
@@ -1,11 +1,11 @@
 
-import UpDate from "./Controls/UpDate";
-import UpDateTime from "./Controls/UpDateTime";
-import UpFile from "./Controls/UpFile";
-import UpSelect2 from "./Controls/UpSelect2";
-import UpStringMultiLine from "./Controls/UpStringMultiLine";
-import UpSwitch from "./Controls/UpSwitch";
-import UpTimePicker from "./Controls/UpTimePicker";
+import UpDate from "./components/UpDate";
+import UpDateTime from "./components/UpDateTime";
+import UpFile from "./components/UpFile";
+import UpSelect2 from "./components/UpSelect2";
+import UpStringMultiLine from "./components/UpStringMultiLine";
+import UpSwitch from "./components/UpSwitch";
+import UpTimePicker from "./components/UpTimePicker";
 
 var _export = {
     UpDate: UpDate,
