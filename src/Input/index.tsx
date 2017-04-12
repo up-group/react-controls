5931cdaf44726da901388e2161b8711be38e3fee
SunTzu <171946@supinfo.com>
2017-04-12 11:59:31 +0200

    Input extends basecontrol

--- src/Input/index.tsx
+++ src/Input/index.tsx
@@ -1,51 +1,81 @@
 import * as React from 'react';
-import { TextInputComponent, EmailInputComponent} from './styles';
-import { Size } from './types';
+import { TextInputComponent, EmailInputComponent } from './styles';
+import { Size, InputType } from './types';
+import { BaseControl } from '../BaseControl/BaseControl';
+import  TypeStringControl  from '../BaseControl/errorCentral/TypeStringControl';
 
-export interface Props extends React.HTMLProps<HTMLInputElement & Input> {
-  color?: string;
-  backgroundColor?: string;
-  fontSize?: Size;
-  isEmail?: boolean;
-  borderColor?: string;
+
+export interface Props extends React.HTMLProps<HTMLInputElement> {
+    color?: string;
+    backgroundColor?: string;
+    fontSize?: Size;
+    borderColor?: string;
+    type?: InputType;
 }
 
-class Input extends React.Component<Props, undefined> {
-  public static defaultProps: Props = {
-    color: '#fefefe',
-    backgroundColor: '#c05b4d',
-    borderColor: '#732419',
-    fontSize: 'medium',
-    isEmail: false
-  };
-  public render() {
-    if (this.props.isEmail) {
-      return (
-        <EmailInputComponent
-          type="email"
-          style={this.props.style}
-          borderColor={this.props.borderColor}
-          onClick={this.props.onClick}
-          color={this.props.color}
-          backgroundColor={this.props.backgroundColor}
-          fontSize={this.props.fontSize}
-        >
-          {this.props.children}
-        </EmailInputComponent>
-      );
+
+export default class Input extends BaseControl<Props> {
+    public static defaultProps: Props = {
+        color: '#fefefe',
+        backgroundColor: '#c05b4d',
+        borderColor: '#732419',
+        fontSize: 'medium',
+    };
+
+    constructor(p, c) {
+        super(p, c);
+
+        var pattern = null;
+        var patternErrorMessage = null;
+
+        if (this.props.pattern != null ) {
+             pattern = new RegExp(this.props.pattern);
+             patternErrorMessage = "test";
+        }
+        else {
+
+            if (this.props.type === "email") {
+                pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                patternErrorMessage = "Doit être un mail";
+            }
+        }
+
+        this._ControlErrorCentral.addControl(new TypeStringControl(pattern, patternErrorMessage));
+
     }
-    return (
-      <TextInputComponent
-        type="text"
-        onClick={this.props.onClick}
-        color={this.props.color}
-        backgroundColor={this.props.backgroundColor}
-        fontSize={this.props.fontSize}
-      >
-        {this.props.children}
-      </TextInputComponent>
-    );
-  }
-}
 
-export default Input;
+    handleChangeJsEvent(event: any) {
+        return event.target.value;
+    }
+
+    public renderControl() {
+        if (this.props.type == "email") {
+            return (
+                <EmailInputComponent
+                    type="email"
+                    style={this.props.style}
+                    borderColor={this.props.borderColor}
+                    onClick={this.props.onClick}
+                    color={this.props.color}
+                    backgroundColor={this.props.backgroundColor}
+                    fontSize={this.props.fontSize}
+                    onChange={this.handleChangeJsEventGlobal} 
+                >
+                    
+                    {this.props.children}
+                </EmailInputComponent>
+            );
+        }
+        return (
+            <TextInputComponent
+                type="text"
+                onClick={this.props.onClick}
+                color={this.props.color}
+                backgroundColor={this.props.backgroundColor}
+                fontSize={this.props.fontSize}
+            >
+                {this.props.children}
+            </TextInputComponent>
+        );
+    }
+}
