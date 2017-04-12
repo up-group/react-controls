a8e99d0b0345a74977bc179648d1b7beaae7ca41
SunTzu <171946@supinfo.com>
2017-04-12 11:54:50 +0200

    render directly inside baseControl to add errorDisplay around the control

--- src/BaseControl/BaseControl.ts
+++
@@ -1,31 +0,0 @@
-import * as React from "react";
-import ControlErrorCentral from "./errorCentral/ControlErrorCentral"
-
-
-export interface baseProp {
-}
-
-export interface baseState {
-}
-
-export abstract class BaseControl<baseType> extends React.Component<baseProp & baseType, baseState> {
-
-    _ControlErrorCentral: ControlErrorCentral;
-
-    constructor(props?, context?) {        super(props, context);        this._ControlErrorCentral = new ControlErrorCentral();    }    abstract handleChangeJsEvent(args: any): baseType;
-
-    public handleChangeJsEventGlobal = (event) => {
-        var cleandata = this.handleChangeJsEvent(event);
-        this.handleChangeEventGlobal(cleandata);
-    }
-
-    public handleChangeEventGlobal = (cleandata) => {
-        var result = this._ControlErrorCentral.isValidValue(cleandata);
-        console.log(result);
-    }
-}
-
-
-
-
-
---
+++ src/BaseControl/BaseControl.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import ControlErrorCentral from "./errorCentral/ControlErrorCentral"
+import ErrorDisplay from "./errorCentral/ErrorDisplay"
+
+
+export interface baseProp {
+}
+
+export interface baseState {
+    error: string
+}
+
+export abstract class BaseControl<baseType> extends React.Component<baseProp & baseType, baseState> {
+
+    _ControlErrorCentral: ControlErrorCentral;
+
+    constructor(props?, context?) {        super(props, context);        this.state = { error: null };        this._ControlErrorCentral = new ControlErrorCentral();    }    public re    abstract handleChangeJsEvent(args: any): baseType;
+    abstract renderControl(): JSX.Element;
+
+    public handleChangeJsEventGlobal = (event) => {
+        var cleandata = this.handleChangeJsEvent(event);
+        this.handleChangeEventGlobal(cleandata);
+    }
+
+    public handleChangeEventGlobal = (cleandata) => {
+        var result = this._ControlErrorCentral.isValidValue(cleandata);
+        console.log(result);
+
+        if (result.hasError) {
+            this.setState({
+                error: result.errorMessage
+            });
+        } else {
+            this.setState({
+                error: null
+            });
+        }
+    }
+
+
+
+    public render() {
+        return <ErrorDisplay error={this.state.error}>
+            {this.renderControl()}
+        </ErrorDisplay>;
+
+    }
+}
+
+
+
+
+
