e55e52342a73d09370b1fd1de5b397f342aa6ae2
Vincent Bouzon <vincent.bouzon@live.fr>
2017-04-14 21:31:25 +0200

    reformat

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ROOT_PATH = path.resolve(__dirname);
 module.exports = {
   devtool: 'cheap-module-source-map',
   entry: {
-      './test/dist/index': './test/index.tsx'
+    './test/dist/index': './test/index.tsx'
   },
   output: {
     filename: '[name].js'
@@ -13,24 +13,24 @@ module.exports = {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
     alias: {
       ui: path.resolve(ROOT_PATH, 'src/'),
-      icons:path.resolve(ROOT_PATH, 'src/theming/icons')
+      icons: path.resolve(ROOT_PATH, 'src/theming/icons')
     },
   },
   module: {
     rules: [
-        { test: /\.tsx?|.ts?$/, loader:  'ts-loader' },
+      { test: /\.tsx?|.ts?$/, loader: 'ts-loader' },
       { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader'], include: path.join(__dirname, 'src') },
       { test: /\.css$/, loader: 'style-loader!css-loader?url=false' },
       { test: /\.json$/, loader: 'json-loader' },
       { test: /\.md$/, loader: 'html!markdown-loader' },
       { test: /\.svg$/, loader: 'svg-inline-loader' },
       {
-          test: /\.(eot|ttf|woff|woff2)$/,
-          loader: 'file-loader'
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader'
       }
     ]
   },
   plugins: [
-     
+
   ]
 };
