72e5a6651e89534569cfdda8994785ad6953f7aa
SunTzu <171946@supinfo.com>
2017-04-14 10:01:08 +0200

    clean dist

--- dist/Article/styles.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var styled_components_1 = require("styled-components");
-var styles_1 = require("../Box/styles");
-exports.default = (_a = ["\n  ", "\n  box-shadow: 0 2px 4px 0 rgba(46, 61, 73, 0.2);\n  border: 1px solid #dbe2e8;\n"], _a.raw = ["\n  ", "\n  box-shadow: 0 2px 4px 0 rgba(46, 61, 73, 0.2);\n  border: 1px solid #dbe2e8;\n"], styled_components_1.default.article(_a, styles_1.BoxStyles));
-var _a;
-//# sourceMappingURL=styles.js.map
