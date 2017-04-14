605117853c915295083e66305963af6abbffd222
Vincent Bouzon <vincent.bouzon@live.fr>
2017-04-14 22:15:42 +0200

    use babel-loader options instead of babelrc file

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const ROOT_PATH = path.resolve(__dirname);
 module.exports = {
   devtool: 'cheap-module-source-map',
   entry: {
-    './dist/up-react-controls' : ['babel-polyfill' , './src/index.ts'],
-    './test/index': ['babel-polyfill' , './test/index.tsx']
+    './dist/up-react-controls': ['babel-polyfill', './src/index.ts'],
+    './test/index': ['babel-polyfill', './test/index.tsx']
   },
   output: {
     filename: '[name].js'
@@ -19,15 +19,21 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.tsx?|.ts?$/, loaders: ['babel-loader', 'ts-loader'] },
+      {
+        test: /\.tsx?|.ts?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: { presets: ['es2015'] }
+          }
+          , 'ts-loader'
+        ]
+      },
       { test: /\.css$/, loader: 'style-loader!css-loader?url=false' },
       { test: /\.json$/, loader: 'json-loader' },
       { test: /\.md$/, loader: 'html!markdown-loader' },
       { test: /\.svg$/, loader: 'svg-inline-loader' },
-      {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        loader: 'file-loader'
-      }
+      { test: /\.(eot|ttf|woff|woff2)$/, loader: 'file-loader' }
     ]
   },
   plugins: [
