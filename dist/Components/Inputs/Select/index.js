1b4b5e19ea7912e57640b346e752f90eae04039f
SunTzu <171946@supinfo.com>
2017-04-18 09:33:09 +0200

    remove folder dist

--- dist/Components/Inputs/Select/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var UpSelect2_1 = require("./UpSelect2");
-exports.default = UpSelect2_1.default;
-//# sourceMappingURL=index.js.map
