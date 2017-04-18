cddb35426559ecbc2ca2d16c86a36e953e0d0375
Vincent Bouzon <vincent.bouzon@live.fr>
2017-04-18 20:12:36 +0200

    fix

--- test/index.tsx
+++ test/index.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 
-
 import {
   UpHeadline,
   UpBox,
@@ -22,9 +21,9 @@ import {
   UpInput,
   UpSelect,
   UpFormGroup
-}
-from "../src/index";
-import {ChangeOrderEvent} from "../src/List/index";
+} from "../src/index";
+
+import { ChangeOrderEvent } from "../src/Components/Containers/List/index";
 
 
 interface Item {
