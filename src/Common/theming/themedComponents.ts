5015348b60aa6dabe58c389d6942aebd18f9e31e
SunTzu <171946@supinfo.com>
2017-04-27 17:27:34 +0200

    modification pour qu'un d.ts puisse être genere

--- src/Common/theming/themedComponents.ts
+++ src/Common/theming/themedComponents.ts
@@ -1,12 +1,18 @@
 import * as styledComponents from "styled-components";
 import { ThemedStyledComponentsModule } from "styled-components";
 
-import {ThemeInterface} from "./types";
+import { ThemeInterface } from "./types";
 
 // Création d'un nouveau module 'styled-components' caster comme utilisant notre interface de theme 
 // afin que tsc l'applique à props.theme lors de l'utilisation des fonctionnalités du module 
 // La notation {default : styled} nomme par 'styled' la propriété 'default' extraite de 'styledComponents'
-const { default: styled, css, injectGlobal, keyframes, ThemeProvider} = styledComponents as ThemedStyledComponentsModule<ThemeInterface>;
+var styled = (styledComponents as ThemedStyledComponentsModule<ThemeInterface>).default;
+var css = (styledComponents as ThemedStyledComponentsModule<ThemeInterface>).css;
+var injectGlobal = (styledComponents as ThemedStyledComponentsModule<ThemeInterface>).injectGlobal;
+var keyframes = (styledComponents as ThemedStyledComponentsModule<ThemeInterface>).keyframes;
+var ThemeProvider = (styledComponents as ThemedStyledComponentsModule<ThemeInterface>).ThemeProvider;
 
 export default styled;
 export { css, injectGlobal, keyframes, ThemeProvider };
+
+    
