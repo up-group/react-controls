9ad85172a9e9af66040379f5b82146341165d18d
SunTzu <171946@supinfo.com>
2017-05-04 11:54:27 +0200

    Ajout du style pour UpSwitch

--- src/Components/Inputs/Switch/UpSwitch.tsx
+++ src/Components/Inputs/Switch/UpSwitch.tsx
@@ -1,29 +1,76 @@
 import * as React from "react"
 
-import {UpSwitchProps, UpSwitchState} from './'
+import { UpSwitchProps, UpSwitchState } from './'
+
+import styled, { css } from '../../../Common/theming/themedComponents';
+
+
 
 export default class UpSwitch extends React.Component<UpSwitchProps, UpSwitchState> {
     constructor(p, c) {
         super(p, c);
-        this.state = { value: this.props.default };
+        this.state = { value: false };
     }
 
     componentDidMount() {
 
     }
 
+
+
     render() {
+
+        var maxWidth = this.props.isNullable ? "50%" : "75%";
+
+        var btnBool: React.CSSProperties = {
+            display: "inline-block",
+            cursor: "pointer",
+            borderRadius: "4px",
+            width: "100%"
+        }
+
+        var btnBoolSpan: React.CSSProperties = {
+            textAlign: "center",
+            color: "#e5e5e5",
+            width: "25%",
+            fontSize: "17px",
+            padding: "2px"
+        }
+
+        var btnFalse: React.CSSProperties = {
+            borderTopLeftRadius: "4px",
+            borderBottomLeftRadius: "4px",
+            float: "left",
+            backgroundColor: "#eba8a6",
+            width: this.state.value === false ? maxWidth : "25%"
+        }
+
+        var btnTrue: React.CSSProperties = {
+            borderTopRightRadius: "4px",
+            borderBottomRightRadius: "4px",
+            float: "right",
+            backgroundColor: "#afd5af",
+            width: this.state.value === true ? maxWidth : "25%"
+        }
+
+        var btnNull: React.CSSProperties = {
+            backgroundColor: "#b2b2b2",
+            float: "left",
+            width: this.state.value === null ? maxWidth : "25%"
+        }
+
+
         if (this.props.isNullable === true) {
-            return <span className="btnBool">
-                <span className={"btnFalse " + (this.state.value === false ? "selected Null" : "")} value={false.toString()} onClick={this.onBoolClick}>Non</span>
-                <span className={"btnNull " + (this.state.value === null ? "selected Null" : "")} value={null} onClick={this.onBoolClick}>Null</span>
-                <span className={"btnTrue " + (this.state.value === true ? "selected Null" : "")} value={true.toString()} onClick={this.onBoolClick}>Oui</span>
+            return <span style={btnBool} className="btnBool">
+                <span style={{ ...btnBoolSpan, ...btnFalse }} value={false.toString()} onClick={this.onBoolClick}>Non</span>
+                <span style={{ ...btnBoolSpan, ...btnNull }} value={null} onClick={this.onBoolClick}>Null</span>
+                <span style={{ ...btnBoolSpan, ...btnTrue }} value={true.toString()} onClick={this.onBoolClick}>Oui</span>
             </span>
         } else {
-            return <span className="btnBool">
-                <span className={" " + (this.state.value === false ? "btnFalse selected" : "btnFalse")} value={false.toString()} onClick={this.onBoolClick}>Non</span>
-                <span className={" " + (this.state.value === true ? "btnTrue selected" : "btnTrue")} value={true.toString()} onClick={this.onBoolClick}>Oui</span>
-            </span>
+            return <span style={btnBool} className="btnBool">
+                <span style={{ ...btnBoolSpan, ...btnFalse }} value={false.toString()} onClick={this.onBoolClick}>Non</span>
+                <span style={{ ...btnBoolSpan, ...btnTrue }} value={true.toString()} onClick={this.onBoolClick}>Oui</span>
+            </span >
         }
     }
 
