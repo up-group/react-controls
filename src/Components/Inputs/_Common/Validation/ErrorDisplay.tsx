688649d6f852a5412585840c54edfc911ebaeb75
SunTzu <171946@supinfo.com>
2017-05-04 12:54:07 +0200

    ajout d'un espace si aucune erreur n'est presente pour avoir une continuite entre les champ

--- src/Components/Inputs/_Common/Validation/ErrorDisplay.tsx
+++ src/Components/Inputs/_Common/Validation/ErrorDisplay.tsx
@@ -14,7 +14,7 @@ export default class ErrorDisplay extends React.Component<ErrorDisplayProps, {}>
     render() {
         return <div>
             {this.props.children}
-            {this.props.showError === true && this.props.hasError === true ? (<span style={{ color: "red" }}>{this.props.error}</span>) : null}
+            {this.props.showError === true && this.props.hasError === true ? (<span style={{ color: "red" }}>{this.props.error}</span>) : "\u00a0"}
 
         </div>
     }
