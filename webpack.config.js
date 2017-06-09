a4fe3851e3d1107df67572adc94854c1bb30b9ab
SunTzu <171946@supinfo.com>
2017-06-09 17:26:01 +0200

    ajout de TimeLine

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,42 @@ const path = require('path');
 const ROOT_PATH = path.resolve(__dirname);
 
 module.exports = {
-  devtool: 'cheap-module-source-map',
-  entry: {
-    './test/dist/index': ['babel-polyfill', './test/index.tsx']
-  },
-  output: {
-    filename: '[name].js'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    alias: {
-      ui: path.resolve(ROOT_PATH, 'src/'),
-      icons: path.resolve(ROOT_PATH, 'src/theming/icons')
+    devtool: 'cheap-module-source-map',
+    entry: {
+        './test/dist/index': ['babel-polyfill', './test/index.tsx']
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: { presets: ['es2015'] }
-          }
-          , 'ts-loader'
+    output: {
+        filename: '[name].js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        alias: {
+            ui: path.resolve(ROOT_PATH, 'src/'),
+            icons: path.resolve(ROOT_PATH, 'src/theming/icons')
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                use: [
+                    {
+                        loader: 'babel-loader'
+                    }
+                    , 'ts-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                loader: ['style-loader', 'css-loader']
+            },
+            { test: /\.json$/, loader: 'json-loader' },
+            { test: /\.md$/, loader: 'html!markdown-loader' },
+            { test: /\.svg$/, loader: 'svg-inline-loader' },
+            { test: /\.(eot|ttf|woff|woff2)$/, loader: 'file-loader?name=fonts/[name].[ext]' }
         ]
-      },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.json$/, loader: 'json-loader' },
-      { test: /\.md$/, loader: 'html!markdown-loader' },
-      { test: /\.svg$/, loader: 'svg-inline-loader' },
-      { test: /\.(eot|ttf|woff|woff2)$/, loader: 'file-loader?name=fonts/[name].[ext]' }
-    ]
-  },
-  plugins: [
+    },
+    plugins: [
 
-  ]
+    ]
 };
