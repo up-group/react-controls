d0a76c9f2dcc4195c9198b0f890fd7406b6274de
SunTzu <171946@supinfo.com>
2017-06-29 15:05:12 +0200

    Les erreur vide n'occupe pas l'espace

--- src/Components/Inputs/_Common/Validation/ErrorDisplay.tsx
+++ src/Components/Inputs/_Common/Validation/ErrorDisplay.tsx
@@ -14,7 +14,7 @@ export default class ErrorDisplay extends React.Component<ErrorDisplayProps, {}>
     render() {
         return <div>
             {this.props.children}
-            {this.props.showError === true && this.props.hasError === true ? (<span style={{ color: "red" }}>{this.props.error}</span>) : "\u00a0"}
+            {this.props.showError === true && this.props.hasError === true ? (<span style={{ color: "red" }}>{this.props.error}</span>) : null}
 
         </div>
     }
