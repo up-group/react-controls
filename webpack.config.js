fbe04dfd55d43a73936445908aa098da5ac4d5ed
Wicusiu <guillaume.chomat@arcan.fr>
2017-08-25 21:04:52 +0200

    Copy SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -42,18 +42,14 @@ module.exports = {
                 options: {
                     name: 'fonts/[name].[ext]'
                 }
-            }
-            ,
+            },
             {
                 test: /\.scss$/,
-                use: ["style-loader" // creates style nodes from JS strings
-                , 
-                     "css-loader" // translates CSS into CommonJS
-                , 
+                use: ["style-loader", // creates style nodes from JS strings
+                     "css-loader", // translates CSS into CommonJS
                      "sass-loader" // compiles Sass to CSS
                 ]
             }
-
         ]
     },
     plugins: [
