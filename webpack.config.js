8ece3b6f0036eb5de1a94a29092a4fa6e88633cb
SunTzu <171946@supinfo.com>
2017-08-25 16:40:50 +0200

    sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,17 @@ module.exports = {
                     name: 'fonts/[name].[ext]'
                 }
             }
+            ,
+            {
+                test: /\.scss$/,
+                use: ["style-loader" // creates style nodes from JS strings
+                , 
+                     "css-loader" // translates CSS into CommonJS
+                , 
+                     "sass-loader" // compiles Sass to CSS
+                ]
+            }
+
         ]
     },
     plugins: [
