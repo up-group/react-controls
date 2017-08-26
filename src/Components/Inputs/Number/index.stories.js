2117f33d8e5d687cc0045f4806f0a13facf7f8cc
Wicusiu <guillaume.chomat@arcan.fr>
2017-08-26 19:31:38 +0200

    Minor fix and stories for number

---
+++ src/Components/Inputs/Number/index.stories.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import UpDefaultTheme from '../../../Common/theming'
+import {IntentType} from '../../../Common/theming/types'
+import { ThemeProvider as UpThemeProvider } from '../../../Common/theming/themedComponents'
+
+import UpNumber from './UpNumber'
+import UpLabel from '../../Display/Label'
+
+var onOptionChange = () => {}
+
+var state = {
+    majeur: false,
+    homme: true,
+    grand: false,
+    couleur: "red",
+    selection: null //{id: 3, text : ""}
+}
+
+storiesOf('UpNumber', module)
+  .addWithInfo('Simple usage', 'Utilisation avec plusieurs options',
+   () => (
+    <UpThemeProvider theme={UpDefaultTheme}>
+        <UpLabel textAlign={"left"} inline={false} width="small" text="Number :">
+            <UpNumber />
+            </UpLabel>
+    </UpThemeProvider>
+  )).addWithInfo('Integer', 'Only greather than 0',
+  () => (
+   <UpThemeProvider theme={UpDefaultTheme}>
+       <UpLabel textAlign={"left"} inline={true} width="small" text="Number :">
+           <UpNumber min={0} />
+           </UpLabel>
+   </UpThemeProvider>
+ )) ;
