2eba7e636247bb967add7ccd03caea986f0355ab
Wicusiu <guillaume.chomat@arcan.fr>
2017-08-29 12:23:47 +0200

    Amélioration ergonomique

--- src/Components/Display/LoadingIndicator/index.tsx
+++ src/Components/Display/LoadingIndicator/index.tsx
@@ -1,15 +1,16 @@
 // Adapted from https://grommet.github.io/docs/spinning
 import * as React from 'react';
 import Box from '../../Containers/Box';
+import { UpGrid, UpRow, UpCol } from '../../Containers/Grid'
 import SvgIcon, { Circle } from './styles';
 
-export type LoadingIndicatorDisplayMode = "inline" | "modal" 
+export type LoadingIndicatorDisplayMode = "inline" | "modal"
 
 export interface LoadingIndicatorProps extends React.Props<typeof LoadingIndicator> {
   isLoading: boolean;
-  displayMode?:LoadingIndicatorDisplayMode;
-  message?:string;
-  title?:string;
+  displayMode?: LoadingIndicatorDisplayMode;
+  message?: string;
+  title?: string;
 }
 export default function LoadingIndicator({
   isLoading,
@@ -21,13 +22,18 @@ export default function LoadingIndicator({
     return null;
   }
 
-  var _displayMode = displayMode ;
-  if(_displayMode==null) {
-    _displayMode = "inline" ;
+  var _displayMode = displayMode;
+  if (_displayMode == null) {
+    _displayMode = "inline";
   }
 
-  var LoadingRender ;
-  if(_displayMode=="inline") {
+  var _title = title ;
+  if (_title == null) {
+    _title = "Veuillez patienter...";
+  }
+
+  var LoadingRender;
+  if (_displayMode == "inline") {
     LoadingRender = () => (<Box
       boxSize={{ horizontal: 'small' }}
       pad="medium"
@@ -44,16 +50,18 @@ export default function LoadingIndicator({
           fill="none"
         />
       </SvgIcon>
-      {message && 
+      {message &&
         <p>{message}</p>
       }
-    </Box>) ;
+    </Box>);
   } else {
     LoadingRender = () => (<aside
-          className="loading-screen" style={{"position":"fixed","top":0,"right":0,"bottom":0,"left":0,"zIndex":9999,backgroundColor:"white","opacity":0.8}}>
-      <div style={{"position":"absolute", "top":"50%", "left":"40%", "marginTop":"-7em"}}>
-          <div className="loading-header">
-            <SvgIcon viewBox="0 0 48 48">
+      className="loading-screen" style={{ "position": "fixed", "top": 0, "right": 0, "bottom": 0, "left": 0, "zIndex": 9999, backgroundColor: "white", "opacity": 0.8 }}>
+      <div style={{ "position": "absolute", "top": "50%", "left": "40%", "marginTop": "-7em" }}>
+        <UpGrid>
+          <UpRow>
+            <UpCol span={6}>
+              <SvgIcon viewBox="0 0 48 48">
                 <Circle
                   cx="24"
                   cy="24"
@@ -63,16 +71,30 @@ export default function LoadingIndicator({
                   fill="none"
                 />
               </SvgIcon>
-              <hgroup style={{"textShadow": "0px 0px 0.1ex"}} className="">
-                  <h3 className="">
-                      <span>{title}</span><br />
-                  </h3>
-              </hgroup>
-              <p  className="loading-status text-info"
-                  style={{"text-align":"center"}}>{message}</p>
-          </div>
+            </UpCol>
+            <UpCol span={18}>
+              <UpGrid>
+                <UpRow>
+                  <UpCol span={24}>
+                    <hgroup style={{ "textShadow": "0px 0px 0.1ex" }} className="">
+                      <h3 style={{ "font-size": "22px" }}>
+                        <span>{_title}</span>
+                      </h3>
+                    </hgroup>
+                  </UpCol>
+                </UpRow>
+                <UpRow>
+                  <UpCol span={24}>
+                    <p className="loading-status text-info"
+                      style={{ "text-align": "left" }}>{message}</p>
+                  </UpCol>
+                </UpRow>
+              </UpGrid>
+            </UpCol>
+          </UpRow>
+        </UpGrid>
       </div>
-      </aside>);
+    </aside>);
   }
 
   return (
