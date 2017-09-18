a990aeeaeed440f33886ab16a00e148c6afbd9d7
Wicusiu <guillaume.chomat@arcan.fr>
2017-09-18 13:04:44 +0200

    Fix définition du style de la largeur

--- src/Components/Inputs/Select/styles.ts
+++ src/Components/Inputs/Select/styles.ts
@@ -4,23 +4,25 @@ import {sizeMap} from '../_Common/Styled'
 import 'react-select/dist/react-select.css'
 import { ThemeInterface } from "../../../Common/theming/types";
 import {UpSelectStyledProps} from './'
-
+console.log(sizeMap) ;
 var getWidth = function(props) {
+    console.log(props.width) ;
+    console.log(sizeMap[props.width]) ;
     switch(props.width) {
       case 'auto':
       return `.Select { width: auto }` ;
       case 'full':
       return `.Select { width: 100% }` ;
       case 'xsmall' :
-      return `.Select { width: ${sizeMap[props.width]? sizeMap[props.width] : "5em" }` ;
+      return `.Select { width: ${sizeMap[props.width]? sizeMap[props.width] : "5em" };` ;
       case 'small':
-      return `.Select { width: ${sizeMap[props.width]? sizeMap[props.width] : "8em" }` ;
+      return `.Select { width: ${sizeMap[props.width]? sizeMap[props.width] : "8em" };` ;
       case 'normal':
-      return `.Select { width: ${sizeMap[props.width]? sizeMap[props.width] : "23em" }` ;
+      return `.Select { width: ${sizeMap[props.width]? sizeMap[props.width] : "23em" };` ;
       case 'large':
-      return `.Select { width: ${sizeMap[props.width]? sizeMap[props.width] : "30em" }` ;
+      return `.Select { width: ${sizeMap[props.width]? sizeMap[props.width] : "30em" };` ;
       default:
-      return `.Select { width: ${sizeMap[props.width]? sizeMap[props.width] : "250px" }` ;
+      return `.Select { width: ${sizeMap[props.width]? sizeMap[props.width] : "250px" };` ;
     }
 }
 
