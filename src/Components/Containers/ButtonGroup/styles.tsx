588dcc9de47e4df413f8be546f173b2fbf199b91
Wicusiu <guillaume.chomat@arcan.fr>
2017-09-18 11:35:32 +0200

    Ajout de la gestion d'un addon

--- src/Components/Containers/ButtonGroup/styles.tsx
+++ src/Components/Containers/ButtonGroup/styles.tsx
@@ -4,19 +4,63 @@ import styled, {css} from '../../../Common/theming/themedComponents';
 import { UpButtonGroupStyledProps} from './'
 import { ThemeInterface } from "../../../Common/theming/types";
 
-const NoGutterStyle = css`
-  .up-btn-wrapper:first-child:not(:last-child):not(.up-dropdown-toggle) .up-btn {
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 0;
+const NoGutterStyle = (props:UpButtonGroupStyledProps) => {
+  switch(props.isAddOn) {
+    case "none": {
+        return css`
+          .up-btn-wrapper:first-child:not(:last-child):not(.up-dropdown-toggle) .up-btn {
+            border-top-right-radius: 0;
+            border-bottom-right-radius: 0;
+          }
+          .up-btn-wrapper:last-child:not(:first-child):not(.up-dropdown-toggle) .up-btn {
+            border-top-left-radius: 0;
+            border-bottom-left-radius: 0;
+          }
+          .up-btn-wrapper:not(:last-child):not(:first-child) .up-btn {
+            border-radius: 0;
+          }
+        `;
+    }
+    case "right": {
+      return css`
+        .up-btn-wrapper:first-child .up-btn {
+          border-top-left-radius: 0;
+          border-bottom-left-radius: 0;
+        }
+        .up-btn-wrapper:first-child:not(:last-child):not(.up-dropdown-toggle) .up-btn {
+          border-top-right-radius: 0;
+          border-bottom-right-radius: 0;
+        }
+        .up-btn-wrapper:last-child:not(:first-child):not(.up-dropdown-toggle) .up-btn {
+          border-top-left-radius: 0;
+          border-bottom-left-radius: 0;
+        }
+        .up-btn-wrapper:not(:last-child):not(:first-child) .up-btn {
+          border-radius: 0;
+        }
+      `;
   }
-  .up-btn-wrapper:last-child:not(:first-child):not(.up-dropdown-toggle) .up-btn {
-    border-top-left-radius: 0;
-    border-bottom-left-radius: 0;
+  case "left": {
+    return css`
+      .up-btn-wrapper:last-child .up-btn {
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+      }
+      .up-btn-wrapper:first-child:not(:last-child):not(.up-dropdown-toggle) .up-btn {
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+      }
+      .up-btn-wrapper:last-child:not(:first-child):not(.up-dropdown-toggle) .up-btn {
+        border-top-left-radius: 0;
+        border-bottom-left-radius: 0;
+      }
+      .up-btn-wrapper:not(:last-child):not(:first-child) .up-btn {
+        border-radius: 0;
+      }
+    `;
   }
-  .up-btn-wrapper:not(:last-child):not(:first-child) .up-btn {
-    border-radius: 0;
-  }
-`;
+} 
+};
 
 var setGutter = (props:UpButtonGroupStyledProps) => {
   if(props.align==='v') {
--- src/Components/Inputs/Button/UpButton.tsx
+++ src/Components/Inputs/Button/UpButton.tsx
@@ -63,114 +63,104 @@ export default class UpButton extends React.Component<UpButtonProps, UpButtonSta
     this.collapse() ;
   }
 
-  public renderButton = () => {
-      const {children, tooltip, onClick, iconName, iconPosition, ...others} = this.props ;
-
-      const BtnList = style({
-        display: this.state.isToggled ? "block" : "none",
-        position: "absolute",
-        top:"35px",
-        zIndex: 1000,
-        listStyle: "none",
-        backgroundColor: "#ffffff",
-        minWidth: 160,
-        margin: 0,
-        paddingTop: 10,
-        paddingBottom: 10,
-        paddingLeft: 0,
-        borderRadius: 4,
-        boxShadow: "0 6px 12px rgba(0, 0, 0, .175)",
-        height: this.state.isToggled ?'auto':'0px',
-        transition: this.state.isToggled ? "height 2s ease-in":"height 2s ease-out",
-        transform: this.state.isToggled ? "scaleY(1)" : "scaleY(0)",    
-        transformOrigin: "top"
-      });
-
-      const buttonElement = style({
-          cursor: "pointer",
-          padding: "8px",
-          $nest: {
-              '&:hover': { backgroundColor: "#f5f5f5" },
-          }
-      });
-
-      const separatorElement = style({
-          height: "1px",
-          margin: "9px 0",
-          overflow: "hidden",
-          backgroundColor: '#e5e5e5'
-      });
-
-      const buttonWrapper = style({
-        display: "inline-block",
-        position: "relative"
-      });
-
-      var icon : boolean | IconName = iconName ;
-      if(icon === false && this.props.dropDown != 'none') {
-        if(this.props.dropDown == 'up') {
-          icon = 'caret-up' ;
-        } else if(this.props.dropDown == 'down') {
-          icon = 'caret-down' ;
-        }
-      }
-
-      var position : IconPosition = iconPosition ;
-      if(position === 'none' && this.props.dropDown != 'none') {
-        position = 'right' ;
-      } else if(position === 'none' && icon === false) {
-        position = 'left' ;
-      }
-
-      return (
-        <div className={classnames('up-btn-wrapper', buttonWrapper)}>
-        <BaseButton iconName={icon} iconPosition={position} onClick={this.handleClick} isToggled={this.state.isToggled} {...others}>
-          {children != null &&
-            <span>{children}</span>
-          }
-        </BaseButton>
-        {this.props.dropDown != 'none' && this.state.isToggled && 
-        <ul tabIndex={0} className={BtnList}>
-          {
-              this.props.extraActions.map((v, i) => {
-                  const isSeparator = this.isSeparator(v) ;
-                  if(!isSeparator) {
-                    return <li tabIndex={i+1} key={i} className={buttonElement}
-                        onMouseDown={this.handleActionClick.bind(this, v)}
-                    >{(v as Action).libelle}</li>
-                  } else {
-                    return <li tabIndex={i+1} role={"separator"} key={i} className={separatorElement}></li>
-                  }
-              })
-          }
-          </ul>
-       }
-        </div>) ;
-  }
-
   isSeparator(element: Action | Separator): boolean {
       return (element as Separator).size !== undefined ;
   }
   
   public render() {
-    const {children, tooltip, onClick, ...others} = this.props ;
+    const {children, tooltip, onClick, iconName, iconPosition, ...others} = this.props ;
     
-    if(tooltip) {
-      var _tooltip:Tooltip ;
-      if(isString(tooltip)) {
-        _tooltip = {
-          content : tooltip as string
-        }
-      } else {
-        _tooltip = tooltip as Tooltip ;
-      }
-      return (
-        <UpTooltip {..._tooltip}>
-         {this.renderButton()}
-        </UpTooltip>
-      );
-    } else {
-      return this.renderButton() ;
-    }
+          const BtnList = style({
+            display: this.state.isToggled ? "block" : "none",
+            position: "absolute",
+            top:"35px",
+            zIndex: 1000,
+            listStyle: "none",
+            backgroundColor: "#ffffff",
+            minWidth: 160,
+            margin: 0,
+            paddingTop: 10,
+            paddingBottom: 10,
+            paddingLeft: 0,
+            borderRadius: 4,
+            boxShadow: "0 6px 12px rgba(0, 0, 0, .175)",
+            height: this.state.isToggled ?'auto':'0px',
+            transition: this.state.isToggled ? "height 2s ease-in":"height 2s ease-out",
+            transform: this.state.isToggled ? "scaleY(1)" : "scaleY(0)",    
+            transformOrigin: "top"
+          });
+    
+          const buttonElement = style({
+              cursor: "pointer",
+              padding: "8px",
+              $nest: {
+                  '&:hover': { backgroundColor: "#f5f5f5" },
+              }
+          });
+    
+          const separatorElement = style({
+              height: "1px",
+              margin: "9px 0",
+              overflow: "hidden",
+              backgroundColor: '#e5e5e5'
+          });
+    
+          const buttonWrapper = style({
+            display: "inline-block",
+            position: "relative"
+          });
+    
+          var icon : boolean | IconName = iconName ;
+          if(icon === false && this.props.dropDown != 'none') {
+            if(this.props.dropDown == 'up') {
+              icon = 'caret-up' ;
+            } else if(this.props.dropDown == 'down') {
+              icon = 'caret-down' ;
+            }
+          }
+    
+          var position : IconPosition = iconPosition ;
+          if(position === 'none' && this.props.dropDown != 'none') {
+            position = 'right' ;
+          } else if(position === 'none' && icon === false) {
+            position = 'left' ;
+          }
+          var _tooltip:Tooltip = null ;
+          if(tooltip) {
+            if(isString(tooltip)) {
+              _tooltip = {
+                content : tooltip as string
+              }
+            } else {
+              _tooltip = tooltip as Tooltip ;
+            }
+          }
+    
+          return (
+            <div className={classnames('up-btn-wrapper', buttonWrapper)}>
+              <UpTooltip {..._tooltip}>
+                <BaseButton iconName={icon} iconPosition={position} onClick={this.handleClick} isToggled={this.state.isToggled} {...others}>
+                  {children != null &&
+                    <span>{children}</span>
+                  }
+                </BaseButton>
+              </UpTooltip>
+              {this.props.dropDown != 'none' && this.state.isToggled && 
+              <ul tabIndex={0} className={BtnList}>
+                {
+                    this.props.extraActions.map((v, i) => {
+                        const isSeparator = this.isSeparator(v) ;
+                        if(!isSeparator) {
+                          return <li tabIndex={i+1} key={i} className={buttonElement}
+                              onMouseDown={this.handleActionClick.bind(this, v)}
+                          >{(v as Action).libelle}</li>
+                        } else {
+                          return <li tabIndex={i+1} role={"separator"} key={i} className={separatorElement}></li>
+                        }
+                    })
+                }
+                </ul>
+              }
+            </div>) ;
   }
 }
