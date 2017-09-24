51b378f116e41e8a12c272c93b764852a857513b
Wicusiu <guillaume.chomat@arcan.fr>
2017-09-24 07:46:19 +0200

    Ajout de badge basé sur IntentType

--- src/Components/Display/Badge/UpBadge.tsx
+++ src/Components/Display/Badge/UpBadge.tsx
@@ -1,46 +1,58 @@
 import * as React from 'react';
 import {style} from 'typestyle';
-
-const BadgeStyle = style({
-  borderRadius:'4px',
-  padding:'6px;',
-  fontWeight : 700
-}) ;
+import {ThemedProps, IntentType} from '../../../Common/theming/types' 
+import {withTheme} from 'styled-components'
 
 export type WidthSize = 'auto' | 'small' | 'medium' | 'large' | 'xlarge' ;
 export type Align = 'left' | 'right'  ;
 
-export interface UpBadgeProps {
+export interface UpBadgeProps extends ThemedProps {
   text:string;
   color?:string;
   background?:string;
+  rounded?:boolean;
+  intent?:IntentType;
 };
-
-export default class UpBadge extends React.Component<UpBadgeProps, {}> {
+ 
+class UpBadge extends React.Component<UpBadgeProps, {}> {
   
   public static defaultProps: UpBadgeProps = {
     text:'',
     color:'#FFF',
-    background:"black"
+    background:"black",
+    rounded:false
   }
 
   constructor(props) {
     super(props) ;
   }
-  
-  componentWillUnmount() {
-  }
-
-  componentDidMount() {
-  }
 
   render() {
       const {children, text, color, background, ...others} = this.props ; 
+      var fontColor = color ;
+      var backgroundColor = background ;
+
+      if(this.props.intent !== null) {
+          fontColor = this.props.theme.colorMap[`${this.props.intent}Fg`] ;
+          backgroundColor = this.props.theme.colorMap[`${this.props.intent}`] ;
+      }
+
+      const BadgeStyle = style({
+        borderRadius: (this.props.rounded===true)? '18px':'6px',
+        padding:(this.props.rounded===true)? "7px 12px" : '6px;',
+        fontWeight : 700,
+        color: fontColor,
+        width:(this.props.rounded===true)? '32px':'auto',
+        backgroundColor:backgroundColor
+      }) ;
+
       return (
-        <span className={BadgeStyle} style={{background:background, color:color}}>
+        <span className={BadgeStyle}>
           {text}
           {children}
         </span>
       ) ;
   }
 }
+
+export default withTheme(UpBadge)
