e11a1b39cff7245b32fb47ae81b51d5825bb0aa5
Wicusiu <guillaume.chomat@arcan.fr>
2017-10-05 09:24:16 +0200

    Amélioration mineure sur les stories du composant Tooltip

--- src/Components/Display/Tooltip/index.stories.tsx
+++ src/Components/Display/Tooltip/index.stories.tsx
@@ -12,7 +12,7 @@ import UpDefaultTheme from '../../../Common/theming'
 import { ThemeProvider as UpThemeProvider } from '../../../Common/theming/themedComponents'
 
 const TooltipRenderer = (<div>
-    <p>
+    <p style={{padding:"10px"}}>
         Pour plus d'info, veuillez consulter le site de notre produit...
     </p>
 </div>) ;
@@ -21,13 +21,25 @@ storiesOf('UpTooltip', module)
 .addWithInfo('Simple usage', 'Utilisation du composant en lui passant les données à afficher',
  () => (
     <UpThemeProvider theme={UpDefaultTheme}>
-        <div style={{margin:"10px"}}>
+        <div style={{margin:"30px"}}>
             <UpPanel type={"primary"}>
-                Exemple d'utilisation du composant <code>UpTooltip</code> sur du texte.
-            </UpPanel>   
-            <UpTooltip title={"Détails"} content={TooltipRenderer}>
-                <UpLigne>Placer la souris dessus</UpLigne>
-            </UpTooltip>
+                Exemple d'utilisation du composant 
+                <UpTooltip title={"Détails"} content={TooltipRenderer}>
+                    <UpLigne>&nbsp;<code>&lt;UpTooltip /&gt;</code>&nbsp;</UpLigne>
+                </UpTooltip> sur du texte.
+            </UpPanel>
         </div>
     </UpThemeProvider>
+)).addWithInfo('Sur un lien', 'Utilisation du composant en lui passant les données à afficher',
+() => (
+   <UpThemeProvider theme={UpDefaultTheme}>
+       <div style={{margin:"30px"}}>
+           <UpPanel type={"primary"}>
+               Exemple d'utilisation du composant 
+               <UpTooltip id={"Tooltip"} title={"Détails"} content={TooltipRenderer}>
+                   <a data-for={"Tooltip"} data-tip="React-tooltip">◕‿‿◕</a>
+               </UpTooltip> sur du texte.
+           </UpPanel>
+       </div>
+   </UpThemeProvider>
 ))
