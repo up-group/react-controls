1e98bb38c19a043758b6acad6edef49f9bd9c96d
Wicusiu <guillaume.chomat@arcan.fr>
2017-11-07 14:19:07 +0100

    Amélioration de l'ergonomie des notifications

--- src/Components/Display/Notification/styles.ts
+++ src/Components/Display/Notification/styles.ts
@@ -7,23 +7,28 @@ import {ThemeInterface} from '../../../Common/theming/types'
 
 const colors = (props: UpNotificationStyledProps) => css`
   position:relative;
-  background-color: ${Color(props.theme.colorMap[`${props.status}Light`]).lighten(0.8).hsl().string() || props.theme.colorMap.offwhite};
+  background-color: ${props.theme.colorMap[`${props.status}Dark`] || props.theme.colorMap.black3};
+  color: ${props.theme.colorMap.offwhite};
   p {
-    color: ${props.theme.colorMap.black3};
     font-weight: 500;
     text-align:left;
   }
 `;
 
-const NotificationStyled = styled.div`
-  ${(props: UpNotificationStyledProps) => colors(props)}
-  width:98%;
-  margin:1%;
-  border-radius:4px;
+const icon  = (props: UpNotificationStyledProps) => css`
   svg {
+    fill: ${props.theme.colorMap.offwhite};;
     margin:10px;
     display:inline-block;
-  }
+}`
+
+const NotificationStyled = styled.div`
+  ${(props: UpNotificationStyledProps) => colors(props)}
+  ${(props: UpNotificationStyledProps) => icon(props)}
+  width:98%;
+  margin:1%;
+  padding:8px;
+  border-radius:4px;
 `;
 
 export default NotificationStyled
