5be4593f640b676f0bda95ca5a03aad0aeb122fc
Wicusiu <guillaume.chomat@arcan.fr>
2017-11-13 15:50:03 +0100

    First test for UpButton

---
+++ src/Components/Inputs/Button/UpButton.test.tsx
@@ -0,0 +1,18 @@
+// Imports
+import * as React from 'react';
+import UpButton from './';
+import * as chai from 'chai';
+import * as chaiEnzyme from 'chai-enzyme';
+import { shallow, mount, render } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import * as renderer from 'react-test-renderer'
+
+chai.use(chaiEnzyme());
+
+describe('UpButton', () => {
+  it('should render without throwing an error', function() {
+    var _render = render(<UpButton onClick={() => {}} />) ;
+    var _input = _render.find("button");
+    expect(_input.length).toBe(1);
+  });
+});
