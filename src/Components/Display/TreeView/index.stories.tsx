6d4079bce42e33c321875d7b1fa29e085665c6ee
SunTzu <171946@supinfo.com>
2017-11-20 17:09:05 +0100

    UpTreeView display invisible

--- src/Components/Display/TreeView/index.stories.tsx
+++ src/Components/Display/TreeView/index.stories.tsx
@@ -6,36 +6,56 @@ import UpTreeView from './UpTreeView'
 
 storiesOf('UpTreeView', module)
     .addWithInfo('Simple usage', 'Utilisation du composant en lui passant les données à afficher',
-()=>{
-   return <UpTreeView childMenuItems={[
-        {
-            id: "1",
-            text: "Test 1",
-          
-            // childMenuItems?: MenuItemData[];
-        },
-        {
-            id: "2",
-            text: "Test 2",
-          
-            childMenuItems: [
-                {
-                    id: "3",
-                    text: "Test 3",
-                   
-                    // childMenuItems?: MenuItemData[];
+    () => {
+        return <UpTreeView
+            onBranchClick={console.log}
+            childMenuItems={[
+            {
+                id: "1",
+                text: "Test 1",
+            },
+            {
+                id: "2",
+                text: "Test 2",
+                childMenuItems: [
+                    {
+                        id: "3",
+                        text: "Test 3",
+                    },
+                    {
+                        id: "24",
+                        text: "Test 4",
+                    }
+                ]
+            }
+        ]} />
+    })
 
+    .addWithInfo('Show invisible', 'Utilisation du composant en lui passant les données à afficher',
+    () => {
+        return <UpTreeView
+            onBranchClick={console.log}
+            showInvisible={true}
+            childMenuItems={[
+                {
+                    id: "1",
+                    text: "Test 1",
                 },
                 {
-                    id: "24",
-                    text: "Test 4",
-                 
-                    // childMenuItems?: MenuItemData[];
+                    isVisible: false,
+                    id: "2",
+                    text: "Test 2",
+                    childMenuItems: [
+                        {
+                            id: "3",
+                            text: "Test 3",
+                        },
+                        {
+                            id: "24",
+                            text: "Test 4",
+                        }
+                    ]
                 }
-            ]
-        }
-    ]} />
-
-}
-
-    );
+            ]} />
+    })
+    ;
