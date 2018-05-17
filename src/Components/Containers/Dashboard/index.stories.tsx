42e183d213b69a5966ba8c632dc1431e7459feb7
SMAIL Mehdi <msmail@softeam.com>
2018-05-17 17:04:01 +0200

    Update UpTitle

--- src/Components/Containers/Dashboard/index.stories.tsx
+++ src/Components/Containers/Dashboard/index.stories.tsx
@@ -9,18 +9,14 @@ import { iconClass } from '@blueprintjs/core/dist/common/classes';
 storiesOf('UpTile', module)
     .addWithInfo('Simple usage 1', 'Utilisation du composant en lui passant les données à afficher',
     () => (
-
-            <div>
+        <div>
             <br />
             <UpTile Title="INFOS">
                 <a>Test</a>
                 <a>Test</a>
                 <a>Test</a>
             </UpTile>
-            <br />
-         <UpTile Title="INFOS">Test </UpTile>
-            <br />
-         <UpTile Title="INFOS">Test </UpTile>
-            <br />
-          </div>
+            <UpTile Title="INFOS">Test </UpTile>
+            <UpTile Title="INFOS">Test </UpTile>
+        </div>
     ));
