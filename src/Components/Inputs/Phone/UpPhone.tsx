e862e98ddb922da3bbf7fe2a45b09b61a87acff3
CHOMAT Guillaume <guillaume.chomat@up.coop>
2019-02-08 11:46:11 +0100

    Fix UpPhone

--- src/Components/Inputs/Phone/UpPhone.tsx
+++ src/Components/Inputs/Phone/UpPhone.tsx
@@ -1,37 +1,55 @@
 ﻿// Imports
 import * as React from 'react'
-import { BaseControlComponent } from '../_Common/BaseControl/BaseControl'
+import { BaseControlComponent, BaseControlState } from '../_Common/BaseControl/BaseControl'
 import UpInput from '../Input'
-import { UpPhoneProps } from '.';
+
+import { CommonInputTextProps } from "../_Common/BaseControl/BaseInput"
+import { Validation } from '../Input/types';
 
 // Exports
-export default class UpPhone extends BaseControlComponent<UpPhoneProps, string> {
+export interface UpPhoneProps extends CommonInputTextProps<string> {
+    validation?: Array<Validation>;
+}
+
+// Exports
+export default class UpPhone extends React.Component<UpPhoneProps, BaseControlState<string>> {
     public static defaultProps: UpPhoneProps = {
         showError: true,
         width: "medium",
-        defaultValue: ""
+        defaultValue: "",
+        validation: [{
+            pattern: /^(0|\+33)[1-9]([-. ]?[0-9]{2}){4}$/,
+            errorMessage: "Le champ doit être un numéro de téléphone"
+        }]
     };
 
     constructor(p, c) {
         super(p, c);
+        this.state = {};
     }
-
-    getValue(event: any) {
-        return event;
-    }
-
-    phoneHandleChangeEvent = (event) => {
-        this.handleChangeEvent(event) ;
+    
+    phoneHandleChangeEvent = (event, value, error) => {
+        this.setState({ value }, () => {
+            if(this.props.onChange) {
+                this.props.onChange(event, value, error) ;
+            }
+        }); 
     }   
 
-    renderControl() {
+    get isControlled() {
+        return this.props.value !== undefined;
+    }
+
+    get currentValue() {
+        return this.isControlled ? this.props.value : this.state.value;
+    }
+
+    render() {
         return (
             <UpInput iconName="phone" 
-                validation={[{
-                    pattern: /^(0|\+33)[1-9]([-. ]?[0-9]{2}){4}$/,
-                    errorMessage: "Le champ doit être un numéro de téléphone "
-                }]} 
-                value={this.state.value} onChange={this.phoneHandleChangeEvent} 
+                validation={this.props.validation} 
+                value={this.currentValue} 
+                onChange={this.phoneHandleChangeEvent} 
                 isRequired={this.props.isRequired}
                 showError={this.props.showError} />
         );
--- src/Components/Inputs/Phone/index.ts
+++ src/Components/Inputs/Phone/index.ts
@@ -1,8 +1,4 @@
 // Imports
 import UpPhone from './UpPhone'
-import { CommonInputTextProps } from "../_Common/BaseControl/BaseInput"
-
-// Exports
-export interface UpPhoneProps extends CommonInputTextProps<string> {}
 
 export default UpPhone ;
