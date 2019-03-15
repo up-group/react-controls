b2929133e7e24917af5ebf63e8991438b5e85507
CHOMAT Guillaume <guillaume.chomat@up.coop>
2019-03-15 12:25:29 +0100

    Review sort

--- src/Components/Containers/DataGrid/UpDataGridCellHeader.tsx
+++ src/Components/Containers/DataGrid/UpDataGridCellHeader.tsx
@@ -6,6 +6,8 @@ import UpSvgIcon from '../../Display/SvgIcon'
 import { SortDirection, Column } from './UpDataGrid'
 
 import * as classnames from 'classnames'
+import UpBox from '../Box';
+import { style } from 'typestyle';
 
 export interface UpDataGridCellHeaderState {
     isSorted:boolean;
@@ -19,18 +21,25 @@ export interface UpDataGridCellHeaderProps {
     width?: string;
 }
 
+const headerCellStyles = style({
+    $nest : {
+        '& .up-data-grid-header-cell-icons .up-icon-wrapper' : {
+            height : '8px',
+        },
+        '& .up-data-grid-header-cell-icons' : {
+            marginLeft: '8px',
+        }
+    }
+})
+
 export default class UpDataGridCellHeader extends React.Component<UpDataGridCellHeaderProps, UpDataGridCellHeaderState> {
 
-    static defaultProps : UpDataGridCellHeaderProps = {
-        
-    }
-
-    constructor(props, context) {
+    constructor(props: UpDataGridCellHeaderProps, context) {
         super(props, context);
         this.state = {
-            isSorted:false,
-            sortDirection:null
-        }
+            isSorted: props.column ? props.column.isSorted : false,
+            sortDirection: props.column ? props.column.sortDir : null,
+        };
     }
 
     onCellClick = () => {
@@ -42,29 +51,52 @@ export default class UpDataGridCellHeader extends React.Component<UpDataGridCell
     }
 
     componentWillReceiveProps(newProps: UpDataGridCellHeaderProps) {
-        if(newProps.column.isSorted !== this.state.isSorted) {
+        if (newProps.column.isSorted != null && newProps.column.isSorted !== this.state.isSorted) {
             this.setState({isSorted: newProps.column.isSorted, sortDirection : (newProps.column.isSorted) ? (this.state.sortDirection==null)?"DESC":"ASC" : null }) ;         
         }
     }
 
     render() {
-        let sortIcon = null;
-        if(this.state.sortDirection=="DESC") {
-            sortIcon = "sort-desc" ;
-        } else if(this.state.sortDirection=="ASC") {
-            sortIcon = "sort-asc" ;
-        }
+        const sortAscIcon = "arrow-down";
+        const sortDescIcon = "arrow-up";
+
         var width = "auto" ;
         if(this.props.width) {
             width = this.props.width ;
         }
         return (
-            <th style={{width: width}} className={classnames("up-data-grid-header-cell", (this.props.column.isSortable)?'up-data-grid-sortable':'')}  onClick={this.onCellClick}>
-                {this.props.column.label}
-                {this.state.isSorted &&  sortIcon != null &&
-                    <UpSvgIcon width={10} iconName={sortIcon} />
-                }
-            </th>
-        )
+          <th
+            style={{ width: width }}
+            className={classnames(
+                headerCellStyles,
+                "up-data-grid-header-cell",
+                this.props.column.isSortable
+                    ? "up-data-grid-sortable"
+                    : ""
+            )}
+            onClick={this.onCellClick}
+          >
+            <UpBox flexDirection={"row"} justifyContent={'flex-start'} alignItems={'center'}>
+                <span className={"up-data-grid-header-cell-label"}>{this.props.column.label}</span>
+                <UpBox className={"up-data-grid-header-cell-icons"} flexDirection={"column"} style={{width:'auto'}}>
+                    <UpSvgIcon
+                        width={8}
+                        iconName={sortDescIcon}
+                        color={
+                        this.state.isSorted &&
+                        this.state.sortDirection == "DESC"
+                            ? "black"
+                            : "#D7D7D7"
+                        }
+                    />
+                    <UpSvgIcon width={8} iconName={sortAscIcon} color={
+                        this.state.isSorted &&
+                            this.state.sortDirection == "ASC"
+                            ? "black"
+                            : "#D7D7D7"
+                    }/>
+                </UpBox></UpBox>
+          </th>
+        );
     }
 }
