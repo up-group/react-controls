7dd3bd9de6526eed5879fa2e4191e5a3b6b7b0f1
CHOMAT Guillaume <guillaume.chomat@up.coop>
2019-03-21 14:17:44 +0100

    Fix local_exec prop

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -33,10 +33,7 @@ resource "azurerm_storage_account" "sa-web" {
     SubProject       = "React-Controls"
   }
    provisioner "local-exec" {
-    inline = [ 
-      "az login  --service-principal -u \"${var.armclientid}\" -p \"${var.armclientsecret}\" --tenant \"${var.armtenantid}\"",
-      "az storage blob service-properties update --account-name ${azurerm_storage_account.sa-web.name} --static-website  --index-document index.html --404-document index.html"
-    ]
+     command = "az login  --service-principal -u \"${var.armclientid}\" -p \"${var.armclientsecret}\" --tenant \"${var.armtenantid}\" | az storage blob service-properties update --account-name ${azurerm_storage_account.sa-web.name} --static-website  --index-document index.html --404-document index.html"
   }
 }
 
