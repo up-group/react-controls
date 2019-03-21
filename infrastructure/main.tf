251b5ff9c6f1722b346116850c89fd3a56c9d6df
CHOMAT Guillaume <guillaume.chomat@up.coop>
2019-03-21 06:51:39 +0100

    Fix bad props

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,43 +18,18 @@ resource "azurerm_storage_account" "sa-web" {
   name                      = "reactcontrols${var.env}sa"
   location                  = "${azurerm_resource_group.rg.location}"
   resource_group_name       = "${azurerm_resource_group.rg.name}"
-  app_service_plan_id       = "${data.azurerm_app_service_plan.asp.id}"
   account_tier              = "Standard"
   account_replication_type  = "GRS"
 
-  properties = {
-    networkAcls = {
-      bypass              = "AzureServices"
-      virtualNetworkRules = []
-      ipRules             = []
-      defaultAction       = "Allow"
-    }
 
-    supportsHttpsTrafficOnly = true
-
-    encryption = {
-      services = {
-        file = {
-          enabled = true
-        }
-
-        blob = {
-          enabled = true
-        }
-      }
-
-      keySource = "Microsoft.Storage"
-    }
-
-    accessTier = "Hot"
-  }
-
-  dependsOn = []
+  
+  access_tier= "Hot"
+  depends_on = []
 
   tags = {
-    Project          = "ODI"
+    Project          = "Up.Public"
     Environment      = "${var.env}"
-    OrganizationName = "ODIFRAN"
-    SubProject       = "Commerçant"
+    OrganizationName = "UPPUBLIC"
+    SubProject       = "React-Controls"
   }
 }
