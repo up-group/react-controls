c77d22d8dd886a6957ea45d1ad201e58714570b5
CHOMAT Guillaume <guillaume.chomat@up.coop>
2019-03-21 14:44:43 +0100

    Use az login  --subscription

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_storage_account" "sa-web" {
     SubProject       = "React-Controls"
   }
    provisioner "local-exec" {
-     command = "az login  --service-principal -u \"${var.armclientid}\" -p \"${var.armclientsecret}\" --tenant \"${var.armtenantid}\" | az storage blob service-properties update --account-name ${azurerm_storage_account.sa-web.name} --static-website  --index-document index.html --404-document index.html"
+     command = "az login  --subscription \"${var.armsubscriptionid}\" -u \"${var.armclientid}\" -p \"${var.armclientsecret}\" --tenant \"${var.armtenantid}\" | az storage blob service-properties update --account-name ${azurerm_storage_account.sa-web.name} --static-website  --index-document index.html --404-document index.html"
   }
 }
 
