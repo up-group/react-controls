b340eb7819cf8a4f6857aa41ffa409d1afda5e9e
Clément Agil <clement.agil@ai3.fr>
2019-09-04 11:11:26 +0200

    implement UpInput test

--- src/Components/Inputs/Input/__tests__/UpInput.test.tsx
+++ src/Components/Inputs/Input/__tests__/UpInput.test.tsx
@@ -20,4 +20,46 @@ describe('UpInput', () => {
     var _input = _render.find("div.up-input-group").parent();
     expect(_input.length).toBe(1);
   });
+
+  it('should display an help message', function() {
+    const message= "Vous devez renseigner un email valide";
+    var _render = mount(<UpInput helpMessage= {message} />) ;
+    expect(_render.find('.up-wrapper-help-message-inline').length).toBe(1);
+    expect(_render.containsMatchingElement(<div>{message}</div>)).toEqual(true);
+  });
+
+  it('should display a custom help message', function() {
+    const message= "Vous devez renseigner un email valide";
+    var _render = mount(<UpInput  helpMessage={children => (
+      <div>
+        {children}
+        <div className={"up-wrapper-help-message-inline-custom"}>
+         {message}
+        </div>
+      </div>
+    )} />) ;
+    expect(_render.find('.up-wrapper-help-message-inline-custom').length).toBe(1);
+    expect(_render.containsMatchingElement(<div>{message}</div>)).toEqual(true);
+  });
+
+  it('should display an inline error', function() {
+    const errorMessage= "Ce champs est obligatoire";
+    var _render = mount(<UpInput hasError={true} showError={true} error={errorMessage} errorDisplayMode={"inline"}/> ) ;
+    expect(_render.find('.up-wrapper-error-inline').length).toBe(1);
+    expect(_render.containsMatchingElement(<div>{errorMessage}</div>)).toEqual(true);
+  });
+
+  it('should not display an inline error', function() {
+    const errorMessage= "Ce champs est obligatoire";
+    var _render = mount(<UpInput hasError={true} showError={false} error={errorMessage} errorDisplayMode={"inline"}/> ) ;
+    expect(_render.find('.up-wrapper-error-inline').length).toBe(0);
+    expect(_render.containsMatchingElement(<div>{errorMessage}</div>)).toEqual(false);
+  });
+
+  it('should not display a tooltip error', function() {
+    const errorMessage= "Ce champs est obligatoire";
+    var _render = render(<UpInput hasError={true} showError={true} error={errorMessage} errorDisplayMode={"tooltip"}/> ) ;
+    expect(_render.find('.up-wrapper-error-tooltip').length).toBe(1);
+  });
+
 });
