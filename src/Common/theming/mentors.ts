9fc93eb7e49279050b3709a45efbc0be25cf1c11
Clément Agil <clement.agil@ai3.fr>
2020-01-22 10:54:41 +0100

    add mentor

--- src/Common/theming/mentors.ts
+++ src/Common/theming/mentors.ts
@@ -5,23 +5,25 @@ export type MentorName =
   | "wink-grey"
   | "happy-orange"
   | "confused-orange"
-  | "sparkles";
+  | "sparkles"
+  | "cry";
 
 export const MentorNames: MentorName[] = [
-         "glasses-orange",
-         "glasses-brown",
-         "mouth-straight-orange",
-         "wink-grey",
-         "happy-orange",
-         "confused-orange",
-         "sparkles"
-       ];
+  "glasses-orange",
+  "glasses-brown",
+  "mouth-straight-orange",
+  "wink-grey",
+  "happy-orange",
+  "confused-orange",
+  "sparkles",
+  "cry"
+];
 
 let dictionary = {};
 
 for (let i = 0; i < MentorNames.length; i++) {
-    const iconName = MentorNames[i];
+  const iconName = MentorNames[i];
   dictionary[iconName] = require("./icons/mentors/" + iconName + ".svg");
 }
 
-export default dictionary
+export default dictionary;
