98d0c727ea89b6bd4baf9da8a0af2eabb7a09e82
Mohamed Loukili <mohamed.loukili@ai3.fr>
2020-02-28 14:42:42 +0100

    add illustrations

--- src/Common/theming/illustrations.ts
+++ src/Common/theming/illustrations.ts
@@ -1,55 +1,59 @@
 export type IllustrationName =
-    | "email-expired"
-    | "email-validation"
-    | "email-verification"
-    | "user-succes"
-    | "user-remove"
-    | "user-creation"
-    | "attachment-succes"
-    | "product-order"
-    | "card-new"
-    | "cadhoc-card"
-    | "cadhoc-check"
-    | "cadhoc-success"
-    | "gift-opened"
-    | "up-dej-card"
-    | "up-dej-card-add"
-    | "up-dej-card-transaction"
-    | "user-avatar"
-    | "card-4c"
-    | "visa"
-    | "mastercard"
-    | "maestro";
+  | "logo-up"
+  | "email-expired"
+  | "email-validation"
+  | "email-verification"
+  | "user-succes"
+  | "user-remove"
+  | "user-creation"
+  | "attachment-succes"
+  | "product-order"
+  | "card-new"
+  | "load-file"
+  | "cadhoc-card"
+  | "cadhoc-check"
+  | "cadhoc-success"
+  | "gift-opened"
+  | "up-dej-card"
+  | "up-dej-card-add"
+  | "up-dej-card-transaction"
+  | "user-avatar"
+  | "card-4c"
+  | "visa"
+  | "mastercard"
+  | "maestro";
 
 export const IllustrationNames: IllustrationName[] = [
-         "email-expired",
-         "email-validation",
-         "email-verification",
-         "user-succes",
-         "user-remove",
-         "user-creation",
-         "attachment-succes",
-         "product-order",
-         "card-new",
-         "cadhoc-card",
-         "cadhoc-check",
-         "cadhoc-success",
-         "gift-opened",
-         "up-dej-card",
-         "up-dej-card-add",
-         "up-dej-card-transaction",
-         "user-avatar",
-         "card-4c",
-         "visa",
-         "mastercard",
-         "maestro"
-       ];
+  "logo-up",
+  "email-expired",
+  "email-validation",
+  "email-verification",
+  "user-succes",
+  "user-remove",
+  "user-creation",
+  "attachment-succes",
+  "product-order",
+  "card-new",
+  "load-file",
+  "cadhoc-card",
+  "cadhoc-check",
+  "cadhoc-success",
+  "gift-opened",
+  "up-dej-card",
+  "up-dej-card-add",
+  "up-dej-card-transaction",
+  "user-avatar",
+  "card-4c",
+  "visa",
+  "mastercard",
+  "maestro"
+];
 
 let dictionary = {};
 
 for (let i = 0; i < IllustrationNames.length; i++) {
-    const iconName = IllustrationNames[i];
+  const iconName = IllustrationNames[i];
   dictionary[iconName] = require("./icons/illustrations/" + iconName + ".svg");
 }
 
-export default dictionary
+export default dictionary;
