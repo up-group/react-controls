1d185b123d99a3e6df419fd7597a312c46087506
Guillaume CHOMAT <guillaume.chomat@up.coop>
2020-03-23 08:28:06 +0100

    Fix padding on UpSelect

--- src/Components/Inputs/Select/styles.ts
+++ src/Components/Inputs/Select/styles.ts
@@ -6,6 +6,7 @@ var getHeight = (props: UpSelectStyledProps) => {
     return {
       $nest : {
         '& > div > div': { minHeight: '35px' },
+        '& > div > div > div': { padding: '2px 8px 2px 0px' },
       }
     }
 }
