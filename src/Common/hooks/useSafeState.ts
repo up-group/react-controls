43a9dea4070db94b7f502f1ad7036763915aa9ea
Guillaume CHOMAT <guillaume.chomat@up.coop>
2020-03-26 08:32:52 +0100

    Add use safe state with reducer

--- src/Common/hooks/useSafeState.ts
+++ src/Common/hooks/useSafeState.ts
@@ -2,10 +2,31 @@ import * as React from 'react'
 import useMountedRef from './useMountedRef'
 
 const useSafeState = (initialValue) => {
-    const [state, setState] = React.useState(initialValue) ;
+
+    let [state, setState] = React.useState(initialValue) ;
     const mountedRef = useMountedRef() ;
 
     return [state, (value) => {
+        mountedRef.current && setState(value)
+    }]
+}
+
+export const useSafeStateWithReducer = (initialValue) => {
+
+    let [state, setState] = React.useReducer((state, newState) => ({...state, ...newState}), initialValue)
+    const mountedRef = useMountedRef() ;
+
+    return [state, (value) => {
+        mountedRef.current && setState(value)
+    }]
+}
+
+export const useSafeStateWithReducerGeneric = <T>(initialValue:T) => {
+
+    let [state, setState] = React.useReducer((state:T, newState:T) => ({...state, ...newState}), initialValue)
+    const mountedRef = useMountedRef() ;
+
+    return [state, (value:T) => {
         mountedRef.current && setState(value)
     }]
 }
