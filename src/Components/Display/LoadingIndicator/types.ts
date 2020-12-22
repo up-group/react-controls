dc1ee72e6b2767fcbcd178e63e54f2f7f0bb6302
DESKTOP-5BOU3QA\admin <amine.allouti.ext@up.coop>
2020-12-22 21:17:15 +0100

    documentation composant UpLoadingIndictor

---
+++ src/Components/Display/LoadingIndicator/types.ts
@@ -0,0 +1,14 @@
+export type LoadingIndicatorDisplayMode = 'inline' | 'layer' | 'modal' | 'zone';
+
+export type SVGProps = React.SVGAttributes<SVGSVGElement>;
+
+export interface LoadingIndicatorProps {
+    isLoading?: boolean;
+    displayMode?: LoadingIndicatorDisplayMode;
+    message?: string;
+    title?: string;
+    className?: string;
+    width?: number;
+    height?: number;
+    loaderSize?: number;
+};
