5e523cf5604618d2eb62c304ce32a3045cd8121f
DESKTOP-5BOU3QA\admin <amine.allouti.ext@up.coop>
2020-12-22 15:43:57 +0100

    Documentation UpDataPanel

--- src/Components/Containers/DataPanel/UpDataPanel.tsx
+++ src/Components/Containers/DataPanel/UpDataPanel.tsx
@@ -1,63 +1,26 @@
 import * as React from 'react';
-import * as classnames from 'classnames';
-
-import { IntentType } from '../../../Common/theming/types';
-import { ActionType } from '../../../Common/actions';
 import UpDataPanelItem from './UpDataPanelItem';
-import withTheme, {
-  
-} from '../../../Common/theming/withTheme';
-import { ICellFormatter } from '../DataGrid/UpDefaultCellFormatter';
+import withTheme, { WithThemeProps } from '../../../Common/theming/withTheme';
+import { UpDataPanelProps } from './types';
 
-export interface Column {
-  label: string  | JSX.Element;
-  field?: string ;
-  formatter?: ICellFormatter;
-  getFormatterProps?: (value: any) => any;
-  tooltip?: {
-    title?: JSX.Element | string;
-    content: JSX.Element | string;
-  };
-}
-export interface Action {
-  type: ActionType;
-  intent?: IntentType;
-  description?: string;
-  action: (panelValue) => void;
-  libelle?: string;
-}
+const UpDataPanel: React.FunctionComponent<UpDataPanelProps & WithThemeProps> = props => {
+    const { data, ...rest } = props;
 
-export type DisplayType = 'row' | 'column';
-export interface TitleFormatter {
-    format: (row:{}, additionalProps?:any) => React.ReactElement<any>;
-  }
-export interface UpDataPanelProps {
-  columns: Array<Column>;
-  data: Array<any>;
-  showOnlyNotEmptyValue?: boolean;
-  actions?: Array<Action>;
-  displayMode?: DisplayType;
-  className?: string;
-  title?: {
-    general: TitleFormatter | JSX.Element | string;
-    specific?: TitleFormatter | JSX.Element | string;
-  };
-}
-
-
-export const UpDataPanel = props => {
-  const { data, ...rest } = props;
-
-  return (
-    data &&
-    data.map((panelElement, index) => {
-      return (
-        <React.Fragment key={index}>
-          <UpDataPanelItem panelData={panelElement} {...rest} />
-        </React.Fragment>
-      );
-    })
-  );
+    return (
+        <>
+            {data && data.map((panelElement, index) => (
+                <React.Fragment key={index}>
+                    <UpDataPanelItem panelData={panelElement} {...rest} />
+                </React.Fragment>
+            ))}
+        </>
+    )
 };
 
+UpDataPanel.defaultProps = {
+    displayMode: 'row',
+    showOnlyNotEmptyValue: false
+};
+
+export { UpDataPanel };
 export default withTheme<UpDataPanelProps>(UpDataPanel);
