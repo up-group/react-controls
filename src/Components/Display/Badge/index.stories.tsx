8bf17b604361c68f7f609be083cf2a4fd1fcdb32
DESKTOP-5BOU3QA\admin <amine.allouti.ext@up.coop>
2020-12-23 21:48:48 +0100

    clean story code

--- src/Components/Display/Badge/index.stories.tsx
+++ src/Components/Display/Badge/index.stories.tsx
@@ -6,42 +6,43 @@ import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 
 export default {
     title: 'Components/Display/UpBadge',
-    decorators: [withKnobs, getRootContainer('UpBadge')],
+    decorators: [
+        withKnobs,
+        getRootContainer('UpBadge'),
+    ],
     component: UpBadgeComponent
 };
 
 export const General =
     () => (
-        <UpBox style={{ margin: '30px' }}>
-            <dl>
-                <dt>Badge avec <code>intent</code> définit à <code>primary</code></dt>
-                <dd>
-                    <UpBadge text="1" rounded={true} intent="primary" /><br/>
-                </dd>
-                <dt>Badge avec <code>intent</code> définit à <code>info</code></dt>
-                <dd>
-                    <UpBadge text="2" rounded={true} intent="info" /><br/>
-                </dd>
-                <dt>Badge avec <code>intent</code> définit à <code>default</code></dt>
-                <dd>
-                    <UpBadge text="3" rounded={true} intent="default" /><br/>
-                </dd>
-                <dt>Badge avec <code>intent</code> définit à <code>warning</code></dt>
-                <dd>
-                    <UpBadge text="4" rounded={true} intent="warning" /><br/>
-                </dd>
-                <dt>Badge avec <code>intent</code> définit à <code>danger</code></dt>
-                <dd>
-                    <UpBadge text="5" rounded={true} intent="danger" /><br/>
-                </dd>
-                <dt>Badge avec <code>intent</code> définit à <code>error</code></dt>
-                <dd>
-                    <UpBadge text="6" rounded={true} intent="error" /><br/>
-                </dd>
-                <dt>Badge avec <code>intent</code> définit à <code>light</code></dt>
-                <dd>
-                    <UpBadge text="7" rounded={true} intent="light" />
-                </dd>
-            </dl>
-        </UpBox>
-    )
+        <dl>
+            <dt>Badge avec <code>intent</code> définit à <code>primary</code></dt>
+            <dd>
+                <UpBadge text="1" rounded={true} intent="primary" /><br />
+            </dd>
+            <dt>Badge avec <code>intent</code> définit à <code>info</code></dt>
+            <dd>
+                <UpBadge text="2" rounded={true} intent="info" /><br />
+            </dd>
+            <dt>Badge avec <code>intent</code> définit à <code>default</code></dt>
+            <dd>
+                <UpBadge text="3" rounded={true} intent="default" /><br />
+            </dd>
+            <dt>Badge avec <code>intent</code> définit à <code>warning</code></dt>
+            <dd>
+                <UpBadge text="4" rounded={true} intent="warning" /><br />
+            </dd>
+            <dt>Badge avec <code>intent</code> définit à <code>danger</code></dt>
+            <dd>
+                <UpBadge text="5" rounded={true} intent="danger" /><br />
+            </dd>
+            <dt>Badge avec <code>intent</code> définit à <code>error</code></dt>
+            <dd>
+                <UpBadge text="6" rounded={true} intent="error" /><br />
+            </dd>
+            <dt>Badge avec <code>intent</code> définit à <code>light</code></dt>
+            <dd>
+                <UpBadge text="7" rounded={true} intent="light" />
+            </dd>
+        </dl>
+    );
