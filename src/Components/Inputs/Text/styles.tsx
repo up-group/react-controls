282f72e5884e385c0bacdb45e9ab522da07629fc
DESKTOP-5BOU3QA\admin <amine.allouti.ext@up.coop>
2021-01-13 16:07:52 +0100

    documentation UpText

--- src/Components/Inputs/Text/styles.tsx
+++ src/Components/Inputs/Text/styles.tsx
@@ -4,6 +4,7 @@ import { defaultStyles } from '../_Common/Styled/Input/styles'
 import { NestedCSSProperties } from 'typestyle/lib/types';
 import { UpTextProps } from './types';
 import { style } from 'typestyle';
+import { toRem } from '../../../Common/theming/utils';
 
 const sizeMap = {
     xsmall: "100px",
@@ -15,31 +16,31 @@ const sizeMap = {
     fill: "100%",
 };
 
-const base = (props: UpTextProps) : NestedCSSProperties => (
+const base = (props: UpTextProps): NestedCSSProperties => (
     {
-        $nest : {
-            "&.up-text" : { 
-                minHeight:'80px',
+        $nest: {
+            "&.up-text": {
+                minHeight: '80px',
                 width: sizeMap[props.width] || "500px",
-                padding:'10px',
+                padding: toRem(10),
                 border: `1px solid ${props.theme.colorMap.darkGray4}`,
                 borderRadius: props.theme.borderRadius
             },
             "&.up-text::placeholder": {
                 color: props.theme.colorMap.grey1,
                 fontStyle: 'italic',
-                fontSize:'14px',
+                fontSize: toRem(14),
                 fontWeight: 500
             }
-            
+
         }
     }
 );
 
-const error = (props: UpTextProps) : NestedCSSProperties => ({ 
-  border : `1px solid ${props.theme.colorMap.danger}`,
+const error = (props: UpTextProps): NestedCSSProperties => ({
+    border: `1px solid ${props.theme.colorMap.danger}`,
 });
 
-export const getStyles =(props: UpTextProps) : string => (
+export const getStyles = (props: UpTextProps): string => (
     classnames(style(defaultStyles(props)), style(base(props)), props.hasError ? style(error(props)) : null)
 );
