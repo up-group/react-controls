8850eb59c32f63a92aa46a954b18d02ea29d6122
DESKTOP-5BOU3QA\admin <amine.allouti.ext@up.coop>
2021-01-15 16:21:55 +0100

    Documentation UpDate

--- src/Components/Inputs/Date/UpDate.tsx
+++ src/Components/Inputs/Date/UpDate.tsx
@@ -1,268 +1,221 @@
-// Imports
-import "normalize.css/normalize.css"
+import 'normalize.css/normalize.css'
 import 'react-dates/initialize';
-import * as React from "react"
-import { UpDateProps } from './'
-
+import * as React from 'react'
+import { UpDateProps } from './types';
 import { BaseControlComponent } from '../_Common/BaseControl/BaseControl'
-import defaultTheme from "../../../Common/theming";
-import withTheme, { WithThemeProps } from "../../../Common/theming/withTheme";
-import { eventFactory } from "../../../Common/utils/eventListener";
+import defaultTheme from '../../../Common/theming';
+import withTheme, { WithThemeProps } from '../../../Common/theming/withTheme';
+import { eventFactory } from '../../../Common/utils/eventListener';
 import SvgIcon from '../../Display/SvgIcon'
-import { Moment } from "moment";
+import { Moment } from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import { generateUniqueId } from '../../../Common/utils/helpers';
+import defaultPhrases from './i18n/fr';
+import * as classnames from 'classnames';
+import { isEmpty } from '../../../Common/utils';
+import * as moment from 'moment';
+import getStyles from './styles';
+import { style } from 'typestyle';
+import UpSelect from '../Select';
+import { isNumeric } from '../../../Common/utils/helpers';
 
-import { SingleDatePicker } from "react-dates";
-import isInclusivelyAfterDay from '../../../Common/utils/isInclusivelyAfterDay';
-import { generateUniqueId } from "../../../Common/utils/helpers";
+moment.locale('fr');
 
-import defaultPhrases from "./i18n/fr";
-
-import * as classnames from "classnames";
-import { isEmpty } from "../../../Common/utils";
-
-import * as moment from "moment";
-import getStyles from "./styles";
-import { style } from "typestyle";
-import UpSelect from "../Select";
-
-import { isNumeric } from "../../../Common/utils/helpers";
-
-moment.locale("fr");
-
-// Exports
 const MIN_DATE = new Date(-8640000000000);
 const MAX_DATE = new Date(+8640000000000);
 
-const selectStyle = style({
-  $nest: {
-     '& > select' : {
-        display: 'inline-block',
-        padding: '0.5em',
-        backgroundImage: 'linear-gradient(45deg, transparent 50%, gray 50%),linear-gradient(135deg, gray 50%, transparent 50%),linear-gradient(to right, #ccc, #ccc)',
-        backgroundPosition: 'calc(100% - 4px) calc(0.25em + 0.5px), calc(100% - 3.3px) calc(0.25em + 0.5px), calc(100% - 0.6em) 0.13em',
-        backgroundSize: '5px 5px, 5px 5px, 1px 1.5em',
-        backgroundRepeat: 'no-repeat',
-        backgroundColor: 'transparent',
-        border: 0,
-        margin: 0,      
-        '-webkit-box-sizing': 'border-box',
-        '-moz-box-sizing': 'border-box',
-        boxSizing: 'border-box',
-        '-webkit-appearance': 'none',
-        '-moz-appearance': 'none'
-    },
-    '& > select:focus' : {
-      backgroundImage: 'linear-gradient(45deg, green 50%, transparent 50%),linear-gradient(135deg, transparent 50%, green 50%),linear-gradient(to right, #ccc, #ccc)',
-      backgroundPosition: 'calc(100% - 3.3px) 0.25em, calc(100% - 5px) 0.25em, calc(100% - 0.6em) 0.13em;',
-      backgroundSize: '5px 5px, 5px 5px, 1px 1.5em',
-      backgroundRepeat: 'no-repeat',
-      border: 0,
-      borderColor: 'green',
-      zIndex: 99
-    },
-    '& > select:-moz-focusring' : {
-      color: 'transparent',
-      textShadow: '0 0 0 #000',
+class UpDate extends BaseControlComponent<UpDateProps & WithThemeProps, Moment> {
+
+    public static defaultProps: UpDateProps = {
+        format: 'DD/MM/YYYY',
+        showError: true,
+        theme: defaultTheme,
+        minDate: MIN_DATE,
+        maxDate: MAX_DATE,
+        iconPosition: 'right',
+        numberOfMonths: 2,
+        enableOutsideDays: true,
+        daySize: 30
+    };
+
+    dateInput: any;
+    id: string;
+    datePicker: HTMLInputElement;
+
+    constructor(p, c) {
+        super(p, c);
+        this.state = { value: this.props.value };
+        this.id = generateUniqueId();
     }
-  }
-})
 
-class UpDate extends BaseControlComponent<
-  UpDateProps & WithThemeProps,
-  Moment
-> {
-  public static defaultProps: UpDateProps = {
-    format: "DD/MM/YYYY",
-    showError: true,
-    theme: defaultTheme,
-    minDate: MIN_DATE,
-    maxDate: MAX_DATE,
-    iconPosition: 'right',
-    numberOfMonths: 2,
-    enableOutsideDays: true,
-    daySize: 30
-  };
+    onChange = (startDate: Moment, endDate?: Moment) => {
+        this.handleChangeEvent(eventFactory(this.props.name, startDate), startDate);
+    };
 
-  dateInput: any;
-  id: string;
-  datePicker : HTMLInputElement;
-
-  constructor(p, c) {
-    super(p, c);
-    this.state = { value: this.props.value };
-    this.id = generateUniqueId();
-  }
-
-  onChange = (startDate: Moment, endDate?: Moment) => {
-    this.handleChangeEvent(eventFactory(this.props.name, startDate), startDate);
-  };
-
-  showError() {
-    return this.props.showError !== undefined
-      ? this.props.showError === true
-      : this.hasError;
-  }
-
-  showSuccess() {
-    return this.props.showSuccess;
-  }
-
-  setInput = input => {
-    // The ref function is called twice,
-    // the first one with the component instance (as React)
-    // and the second one with the DOM node instance
-    if (this.dateInput == undefined) {
-      this.dateInput = input;
-      if(this.props.tabIndex) {
-        this.dateInput.setAttribute('tavbindex', this.props.tabIndex);
-      }
+    showError() {
+        return this.props.showError !== undefined ? this.props.showError === true : this.hasError;
     }
-  };
 
-  onFocusChange = ({ focused }: { focused: boolean }) => {
-    if (focused) {
-      this.onFocus(eventFactory("focus", true));
-    } else {
-      this.onBlur(eventFactory("blur", true));
+    showSuccess() {
+        return this.props.showSuccess;
     }
-  };
 
-  checkDate (e: KeyboardEvent): void {
-    const event = e.target as HTMLInputElement;
-    
-    if (event && event.value) {
-      const value = event.value as string;
-      const stringsToNumerics = value.split('/').map(stringValue => isNumeric(stringValue));
-      //Check if the array has falsy values which means unwanted characters.
-      const unwantedCharacters = stringsToNumerics.some(booleanValue => !booleanValue);
-      //Two characters are acceptable : / and -
-      if(unwantedCharacters) event.value = event.value.replace(/[a-zA-Z\u00C0-\u024F\u1E00-\u1EFF!"'$%^&*()_+|~=`{}\[\]:\\;<>?,.@#§µ²°]/, '');
+    setInput = input => {
+        // The ref function is called twice,
+        // the first one with the component instance (as React)
+        // and the second one with the DOM node instance
+        if (this.dateInput == undefined) {
+            this.dateInput = input;
+            if (this.props.tabIndex) {
+                this.dateInput.setAttribute('tavbindex', this.props.tabIndex);
+            }
+        }
+    };
+
+    onFocusChange = ({ focused }: { focused: boolean }) => {
+        if (focused) {
+            this.onFocus(eventFactory('focus', true));
+        } else {
+            this.onBlur(eventFactory('blur', true));
+        }
+    };
+
+    checkDate(e: KeyboardEvent): void {
+        const event = e.target as HTMLInputElement;
+
+        if (event && event.value) {
+            const value = event.value as string;
+            const stringsToNumerics = value.split('/').map(stringValue => isNumeric(stringValue));
+            //Check if the array has falsy values which means unwanted characters.
+            const unwantedCharacters = stringsToNumerics.some(booleanValue => !booleanValue);
+            //Two characters are acceptable : / and -
+            if (unwantedCharacters) event.value = event.value.replace(/[a-zA-Z\u00C0-\u024F\u1E00-\u1EFF!''$%^&*()_+|~=`{}\[\]:\\;<>?,.@#§µ²°]/, '');
+        }
     }
-  }
 
-  componentDidMount() {
-    this.datePicker = document.getElementById(this.id) as HTMLInputElement;
+    componentDidMount() {
+        this.datePicker = document.getElementById(this.id) as HTMLInputElement;
 
-    if (this.datePicker) {
-      this.datePicker.addEventListener('input', this.checkDate);
+        if (this.datePicker) {
+            this.datePicker.addEventListener('input', this.checkDate);
+        }
+
+        if (this.props['dataFor'] && this.datePicker) {
+            this.datePicker.setAttribute('data-tip', 'tooltip');
+            this.datePicker.setAttribute('data-for', this.props['dataFor']);
+        }
     }
-    
-    if (this.props["dataFor"] && this.datePicker) {
-      this.datePicker.setAttribute("data-tip", "tooltip");
-      this.datePicker.setAttribute("data-for", this.props["dataFor"]);
+
+    componentWillUnmount() {
+        if (this.datePicker) this.datePicker.removeEventListener('input', this.checkDate);
     }
-  }
 
-  componentWillUnmount(){
-    if (this.datePicker) this.datePicker.removeEventListener('input', this.checkDate);
-  }
-  
-  returnYears = () => {
-    let years = []
-    for(let i = moment().year() - 100; i <= moment().year(); i++) {
-        years.push({
-          id: i,
-          text: i
-        });
+    returnYears = () => {
+        let years = []
+        for (let i = moment().year() - 100; i <= moment().year(); i++) {
+            years.push({
+                id: i,
+                text: i
+            });
+        }
+        years = years.sort((a, b) => b.id - a.id);
+        return years;
     }
-    years = years.sort((a, b) => b.id - a.id);
-    return years;
-  }
 
-  formatMonth = (month) => (month+1) < 10 ? `0${month+1}` : `${month+1}`;
+    formatMonth = (month) => (month + 1) < 10 ? `0${month + 1}` : `${month + 1}`;
 
-  renderMonthElement = ({ month, onMonthSelect, onYearSelect }) =>
-      <div style={{ width: '100%', display: 'flex', justifyContent: 'center', flexShrink: 2 }}>
-        <div style={{ width: '30%' }}>
-          <UpSelect
-              value={{ id: month.month(), text : this.formatMonth(month.month())}}
-              menuPlacement='bottom'
-              data={moment.months().map((label, value) => (
-                {
-                  id: value,
-                  text: label
-                }
-              ))}
-              tooltip={moment.months().find((label, value) => value == month.month())}
-              onChange={(e) => {
-                onMonthSelect(month, e.target.value.id)
-              }}
-          >
-          </UpSelect>
+    getValue(newDate: any) {
+        return newDate;
+    }
+
+    renderMonthElement = ({ month, onMonthSelect, onYearSelect }) => (
+        <div style={{ width: "100%", display: "flex", justifyContent: "center", flexShrink: 2 }}>
+            <div style={{ width: "30%" }}>
+                <UpSelect
+                    value={{ id: month.month(), text: this.formatMonth(month.month()) }}
+                    menuPlacement="bottom"
+                    data={moment.months().map((label, value) => (
+                        {
+                            id: value,
+                            text: label
+                        }
+                    ))}
+                    tooltip={moment.months().find((label, value) => value == month.month())}
+                    onChange={(e) => {
+                        onMonthSelect(month, e.target.value.id)
+                    }}
+                >
+                </UpSelect>
+            </div>
+            <div className={style({ width: "40%" })}>
+                <UpSelect
+                    data={this.returnYears()}
+                    menuPlacement="bottom"
+                    value={{ id: month.year(), text: month.year() }}
+                    isSearchable={true}
+                    onChange={(e) => onYearSelect(month, e.target.value.id)}>
+                </UpSelect>
+            </div>
         </div>
-        <div className={style({ width: '40%' })}>
-          <UpSelect
-              data={this.returnYears()}
-              menuPlacement='bottom'
-              value={{ id : month.year(), text : month.year()}}
-              isSearchable={true}
-              onChange={(e) => onYearSelect(month, e.target.value.id)}>
-          </UpSelect>
-        </div>
-    </div>
+    )
 
-  defaultIsOutsideRange =  (day) : boolean => 
-    (this.props.maxDate && day.toDate().setHours(0,0,0,0) > this.props.maxDate.setHours(0,0,0,0)) || 
-    (this.props.minDate && day.toDate().setHours(0,0,0,0) < this.props.minDate.setHours(0,0,0,0));
+    defaultIsOutsideRange = (day): boolean =>
+        (this.props.maxDate && day.toDate().setHours(0, 0, 0, 0) > this.props.maxDate.setHours(0, 0, 0, 0)) ||
+        (this.props.minDate && day.toDate().setHours(0, 0, 0, 0) < this.props.minDate.setHours(0, 0, 0, 0));
 
-  renderControl() {
-    const {
-      disabled,
-      minDate,
-      maxDate,
-      floatingLabel,
-      placeholder,
-      iconPosition
-    } = this.props;
+    renderControl() {
+        const {
+            disabled,
+            minDate,
+            maxDate,
+            floatingLabel,
+            placeholder,
+            iconPosition
+        } = this.props;
 
-    return (
-      <div
-        className={classnames(
-          getStyles({
-            focused: this.isFocused,
-            ...(this.props as Omit<UpDateProps, "children">)
-          }),
-          "up-date",
-          this.isFocused ? "up-input-focused" : null,
-          !isEmpty(this.currentValue) ? "up-input-valued" : null
-        )}
-      >
-        {floatingLabel && <label htmlFor={this.id}>{floatingLabel}</label>}
-        <SingleDatePicker
-          customInputIcon={<SvgIcon iconName='calendar' width="15px" height="15px" />}
-          enableOutsideDays={this.props.enableOutsideDays}
-          renderMonthElement={this.props.numberOfMonths == 1 ? this.renderMonthElement : null}
-          numberOfMonths={this.props.numberOfMonths}
-          focused={this.isFocused}
-          onFocusChange={this.onFocusChange}
-          placeholder={floatingLabel ? "" : placeholder}
-          date={this.currentValue}
-          onDateChange={this.onChange}
-          id={this.id}
-          disabled={disabled || this.props.readonly}
-          showClearDate={!this.props.readonly}
-          showDefaultInputIcon={true}
-          noBorder={false}
-          screenReaderInputMessage={
-            floatingLabel ? floatingLabel : placeholder ? placeholder : "Date"
-          }
-          ref={this.setInput}
-          keepOpenOnDateSelect={false}
-          hideKeyboardShortcutsPanel={true}
-          phrases={defaultPhrases}
-          isDayBlocked={day => false}
-          inputIconPosition={iconPosition == "right" ? "after" : "before"}
-          isOutsideRange={this.props.isOutsideRange || this.defaultIsOutsideRange}
-          // isDayHighlighted={(day: any) => day == new Date()}
-          daySize={this.props.daySize}
-        />
-      </div>
-    );
-  }
+        return (
+            <div
+                className={classnames(
+                    getStyles({
+                        focused: this.isFocused,
+                        ...(this.props as Omit<UpDateProps, 'children'>)
+                    }),
+                    'up-date',
+                    this.isFocused ? 'up-input-focused' : null,
+                    !isEmpty(this.currentValue) ? 'up-input-valued' : null
+                )}
+            >
+                {floatingLabel && <label htmlFor={this.id}>{floatingLabel}</label>}
+                <SingleDatePicker
+                    customInputIcon={<SvgIcon iconName="calendar" width="15px" height="15px" />}
+                    enableOutsideDays={this.props.enableOutsideDays}
+                    renderMonthElement={this.props.numberOfMonths == 1 ? this.renderMonthElement : null}
+                    numberOfMonths={this.props.numberOfMonths}
+                    focused={this.isFocused}
+                    onFocusChange={this.onFocusChange}
+                    placeholder={floatingLabel ? '' : placeholder}
+                    date={this.currentValue}
+                    onDateChange={this.onChange}
+                    id={this.id}
+                    disabled={disabled || this.props.readonly}
+                    showClearDate={!this.props.readonly}
+                    showDefaultInputIcon={true}
+                    noBorder={false}
+                    screenReaderInputMessage={floatingLabel ? floatingLabel : placeholder ? placeholder : "Date"}
+                    ref={this.setInput}
+                    keepOpenOnDateSelect={false}
+                    hideKeyboardShortcutsPanel={true}
+                    phrases={defaultPhrases}
+                    isDayBlocked={day => false}
+                    inputIconPosition={iconPosition == "right" ? "after" : "before"}
+                    isOutsideRange={this.props.isOutsideRange || this.defaultIsOutsideRange}
+                    // isDayHighlighted={(day: any) => day == new Date()}
+                    daySize={this.props.daySize}
+                />
+            </div>
+        );
+    }
+};
 
-  getValue(newDate: any) {
-    return newDate;
-  }
-}
-
-export default withTheme<UpDateProps>(UpDate) 
+export { UpDate };
+export default withTheme<UpDateProps>(UpDate);
