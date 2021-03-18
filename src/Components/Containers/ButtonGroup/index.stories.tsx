2ad5049848bc99a6bf51fcd1882efde14dec06f6
DESKTOP-5BOU3QA\admin <amine.allouti.ext@up.coop>
2021-03-18 17:22:25 +0100

    MAJ UpButtonGroups Stories

--- src/Components/Containers/ButtonGroup/index.stories.tsx
+++ src/Components/Containers/ButtonGroup/index.stories.tsx
@@ -22,7 +22,10 @@ export default {
 export const DropDown =
     () => (
         <>
-            <UpButtonGroup isAddOn='left' gutter={0} align={'h'}>
+            <UpButtonGroup
+                gutter={0}
+                align={'h'}
+            >
                 <UpButton
                     onClick={action('OnClick')}
                     dropDown='down'
@@ -36,7 +39,7 @@ export const DropDown =
                     BOUTON 3
                 </UpButton>
             </UpButtonGroup>
-            <br/>
+            <br />
             <UpButtonGroup gutter={10} align={'h'}>
                 <UpButton
                     onClick={action('Main')}
@@ -55,85 +58,106 @@ export const DropDown =
         </>
     );
 
-export const MultiButton =
+export const GluedButtons =
     () => (
-        <>
-            <UpButtonGroup gutter={0} align={'h'}>
-                <UpButton
-                    onClick={action('Add')}
-                    width={'normal'}
-                    actionType={'add'}>
-                    Add
-                </UpButton>
-                <UpButton
-                    onClick={action('Edit')}
-                    width={'normal'}
-                    intent={'primary'}
-                    actionType={'edit'}
-                >
-                    Edit
-                </UpButton>
-                <UpButton
-                    onClick={action('Delete')}
-                    width={'normal'}
-                    intent={'danger'}
-                    actionType={'delete'}
-                >
-                    Remove
-                </UpButton>
-            </UpButtonGroup>
-            <br />
-            <UpButtonGroup gutter={5} align={'h'}>
-                <UpButton
-                    intent={'primary'}
-                    onClick={action('Add')} width={'normal'} actionType={'add'}>
-                    Add
-                </UpButton>
-                <UpButton
-                    onClick={action('Edit')}
-                    width={'normal'}
-                    intent={'primary'}
-                    actionType={'edit'}
-                >
-                    Edit
-                </UpButton>
-                <UpButton
-                    onClick={action('Edit')}
-                    width={'normal'}
-                    intent={'info'}
-                    actionType={'info-sign'}
-                >
-                    Info
-                </UpButton>
-                <UpButton
-                    onClick={action('Edit')}
-                    width={'normal'}
-                    intent={'warning'}
-                    actionType={'help'}
-                >
-                    Aide
-                </UpButton>
-                <UpButton
-                    onClick={action('Delete')}
-                    width={'normal'}
-                    intent={'danger'}
-                    actionType={'delete'}
-                >
-                    Remove
-                </UpButton>
-            </UpButtonGroup>
-        </>
+        <UpButtonGroup
+            isAddOn={'left'}
+            gutter={0}
+            align={'h'}
+        >
+            <UpButton
+                onClick={action('Edit')}
+                width={'normal'}
+                intent={'info'}
+                actionType={'info-sign'}
+            >
+                Info
+            </UpButton>
+            <UpButton
+                onClick={action('Edit')}
+                width={'normal'}
+                intent={'primary'}
+                actionType={'edit'}
+            >
+                Edit
+            </UpButton>
+            <UpButton
+                onClick={action('Delete')}
+                width={'normal'}
+                intent={'danger'}
+                actionType={'delete'}
+            >
+                Remove
+            </UpButton>
+        </UpButtonGroup>
     );
 
-export const MultiButtonWithGutter =
+GluedButtons.storyName = 'Show Glued Buttons';
+
+export const VerticalButtons =
+    () => (
+        <UpButtonGroup
+            gutter={5}
+            align={'h'}
+        >
+            <UpButton
+                intent={'primary'}
+                onClick={action('Add')}
+                width={'normal'}
+                actionType={'add'}
+            >
+                Add
+                </UpButton>
+            <UpButton
+                onClick={action('Edit')}
+                width={'normal'}
+                intent={'primary'}
+                actionType={'edit'}
+            >
+                Edit
+                </UpButton>
+            <UpButton
+                onClick={action('Edit')}
+                width={'normal'}
+                intent={'info'}
+                actionType={'info-sign'}
+            >
+                Info
+                </UpButton>
+            <UpButton
+                onClick={action('Edit')}
+                width={'normal'}
+                intent={'warning'}
+                actionType={'help'}
+            >
+                Aide
+                </UpButton>
+            <UpButton
+                onClick={action('Delete')}
+                width={'normal'}
+                intent={'danger'}
+                actionType={'delete'}
+            >
+                Remove
+                </UpButton>
+        </UpButtonGroup>
+    );
+
+VerticalButtons.storyName = 'Show Multiple Buttons Vertically';
+
+export const HorizontalButtons =
     () => (
         <>
-            <UpButtonGroup gutter={20} align={'v'}>
+            <UpButtonGroup
+                gutter={20}
+                align={'v'}
+            >
                 <UpButton
                     intent={'secondary'}
                     onClick={action('Add')}
                     width={'full'}
-                    actionType={'add'}>
+                    actionType={'add'}
+                >
                     Add
                 </UpButton>
                 <UpButton
@@ -156,7 +180,9 @@ export const MultiButtonWithGutter =
         </>
     );
 
-MultiButtonWithGutter.decorators = [
+HorizontalButtons.storyName = 'Show Multiple Buttons Horizontally';
+
+HorizontalButtons.decorators = [
     (MultiButtonWithGutter) =>
         <div style={{
             height: '300px', width: '300px', border: '2px solid #ffc83d', borderRadius: '6px'
