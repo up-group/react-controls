3a08f6a488790c48385a4acf081aeee42e0d76d8
DESKTOP-5BOU3QA\admin <amine.allouti.ext@up.coop>
2021-03-18 15:35:08 +0100

    DOCS && TESTS

---
+++ src/Components/Containers/Panel/__tests__/UpPanel.test.tsx
@@ -0,0 +1,103 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import UpPanel from '../UpPanel';
+import { ThemeProvider as UpThemeProvider } from '../../../../Common/theming/ThemeProvider';
+import UpDefaultTheme from '../../../../Common/theming';
+
+const whithTheme = (component) => <UpThemeProvider theme={UpDefaultTheme}>{component}</UpThemeProvider>;
+const renderComponent = component => render(whithTheme(component));
+
+describe('Tests for UpPanel', () => {
+
+    it('should render title', () => {
+        const { container } = renderComponent(
+            <UpPanel
+                title={'Mon panel'}
+            />
+        );
+
+        expect(container.querySelector('.up-panel_header')).toHaveTextContent('Mon panel');
+    });
+
+    it('should render title as react element', () => {
+        const { getByText } = renderComponent(
+            <UpPanel
+                title={<h3>Mon JSX Panel</h3>}
+            />
+        );
+
+        expect(getByText('Mon JSX Panel').nodeName).toBe('H3');
+        expect(getByText('Mon JSX Panel').parentNode).toHaveClass('up-panel_header');
+    });
+
+    it('should render a message body', () => {
+        const { container } = renderComponent(
+            <UpPanel
+                title={'Mon panel'}
+                message={'Mon panel text'}
+            />
+        );
+
+        expect(container.querySelector('.up-panel_message')).toHaveTextContent('Mon panel text');
+    });
+
+    it('should render footer', () => {
+        const { container } = renderComponent(
+            <UpPanel
+                footer={'this is the footer'}
+            />
+        );
+
+        expect(container.querySelector('.up-panel_footer')).toHaveTextContent('this is the footer');
+    });
+
+    it('should render footer as react element', () => {
+        const { container } = renderComponent(
+            <UpPanel
+                footer={<h3>this is the footer</h3>}
+            />
+        );
+
+        expect(container.querySelector('.up-panel_footer')).toContainHTML('<h3>this is the footer</h3>');
+    });
+
+    it('should not show an icone if truthy disableAutoIntentIcon or no icon is provided', () => {
+        const { container } = renderComponent(
+            <UpPanel
+                type={'default'}
+                title={'Mon Panel'}
+                message={'Mon panel text'}
+                disableAutoIntentIcon= {true}
+            />
+        );
+
+        expect(container.querySelector('.up-panel_message').parentNode).toHaveClass('up-col-24');
+    });
+
+    it('should show an icone if falsy disableAutoIntentIcon', () => {
+        const { container } = renderComponent(
+            <UpPanel
+                type={'warning'}
+                title={'Mon Panel'}
+                message={'Warning Message'}
+                disableAutoIntentIcon={false}
+            />
+        );
+
+        expect(container.querySelector('.up-panel_message').parentNode).toHaveClass('up-col-22');
+    });
+
+    it('should show an icon if iconName property is provided', () => {
+        const { container } = renderComponent(
+            <UpPanel
+                type={'warning'}
+                title={'Mon Panel'}
+                message={'Warning Message'}
+                iconName={'warning'}
+            />
+        );
+
+        expect(container.querySelector('.up-panel_message').parentNode).toHaveClass('up-col-22');
+    });
+});
