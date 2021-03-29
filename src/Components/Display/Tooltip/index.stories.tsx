1e7b6d5a2195657d1982aefbb6a985d9960e9a7d
DESKTOP-5BOU3QA\admin <amine.allouti.ext@up.coop>
2021-03-29 11:17:44 +0200

    Docs && tests

--- src/Components/Display/Tooltip/index.stories.tsx
+++ src/Components/Display/Tooltip/index.stories.tsx
@@ -6,6 +6,10 @@ import UpPanel from '../../Containers/Panel';
 import { getRootContainer } from '../../../Common/stories';
 import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 import { UpSvgIcon } from '../../..';
+import { UpLink } from '../Link/UpLink';
+import { style } from 'typestyle';
+import { UpButton } from '../../..';
+import UpParagraph from '../Paragraph';
 
 export default {
     title: 'Components/Display/UpTooltip',
@@ -14,42 +18,209 @@ export default {
 };
 
 const TooltipRenderer = (
-    <div>
-        <p style={{ padding: '10px' }}>Pour plus d'info, veuillez consulter le site de notre produit...</p>
-    </div>
+    <UpParagraph
+        className={style({ padding: '10px' })}
+        margin={'none'}
+        paragraphSize={'small'}
+    >
+        For more info, please visit our product website...
+    </UpParagraph>
 );
+
+const UpLigneStyleContent = style({
+    fontWeight: 'bold',
+    color: '#fff !important',
+    backgroundColor: '#F59100',
+    padding: '5px',
+    margin: '5px'
+});
 
 export const General =
     () => (
-        <UpPanel type={'primary'}>
-            Exemple d'utilisation du composant
-            <UpTooltip title={'Détails'} content={TooltipRenderer}>
-                <UpLigne>
-                    &nbsp;<code>&lt;UpTooltip /&gt;</code>&nbsp;
-                </UpLigne>
-            </UpTooltip>{' '}
-            sur du texte.
-        </UpPanel>
+        <>
+            <UpPanel type={'primary'}>
+                Panel Type is Light (Default Type)
+                <UpTooltip
+                    title={'Details'}
+                    content={TooltipRenderer}
+                    type={'light'}
+                >
+                    <UpLigne className={UpLigneStyleContent} >
+                        UpTooltip
+                    </UpLigne>
+                </UpTooltip>
+            </UpPanel>
+            <UpPanel type={'danger'}>
+                Panel Type is Danger
+                <UpTooltip
+                    title={'Details'}
+                    content={TooltipRenderer}
+                    type={'danger'}
+                >
+                    <UpLigne className={UpLigneStyleContent} >
+                        UpTooltip
+                    </UpLigne>
+                </UpTooltip>
+            </UpPanel>
+            <UpPanel type={'default'}>
+                Panel Type is Default
+                <UpTooltip
+                    title={'Details'}
+                    content={TooltipRenderer}
+                    type={'default'}
+                >
+                    <UpLigne className={UpLigneStyleContent} >
+                        UpTooltip
+                    </UpLigne>
+                </UpTooltip>
+            </UpPanel>
+            <UpPanel type={'error'}>
+                Panel Type is Error
+                <UpTooltip
+                    title={'Details'}
+                    content={TooltipRenderer}
+                    type={'error'}
+                >
+                    <UpLigne className={UpLigneStyleContent} >
+                        UpTooltip
+                    </UpLigne>
+                </UpTooltip>
+            </UpPanel>
+            <UpPanel type={'info'}>
+                Panel Type is Info
+                <UpTooltip
+                    title={'Details'}
+                    content={TooltipRenderer}
+                    type={'info'}
+                >
+                    <UpLigne className={UpLigneStyleContent} >
+                        UpTooltip
+                    </UpLigne>
+                </UpTooltip>
+            </UpPanel>
+            <UpPanel type={'primary'}>
+                Panel Type is Primary
+                <UpTooltip
+                    title={'Details'}
+                    content={TooltipRenderer}
+                    type={'primary'}
+                >
+                    <UpLigne className={UpLigneStyleContent} >
+                        UpTooltip
+                    </UpLigne>
+                </UpTooltip>
+            </UpPanel>
+            <UpPanel type={'success'}>
+                Panel Type is Success
+                <UpTooltip
+                    title={'Details'}
+                    content={TooltipRenderer}
+                    type={'success'}
+                >
+                    <UpLigne className={UpLigneStyleContent} >
+                        UpTooltip
+                    </UpLigne>
+                </UpTooltip>
+            </UpPanel>
+            <UpPanel type={'warning'}>
+                Panel Type is Warning
+                <UpTooltip
+                    title={'Details'}
+                    content={TooltipRenderer}
+                    type={'warning'}
+                >
+                    <UpLigne className={UpLigneStyleContent} >
+                        UpTooltip
+                    </UpLigne>
+                </UpTooltip>
+            </UpPanel>
+        </>
     );
 
-export const OnALink =
+export const PlaceProperty =
     () => (
-        <UpPanel>
-            Exemple d'utilisation du composant
-            <UpTooltip id={'Tooltip'} title={'Détails'} content={TooltipRenderer}>
-                <a data-for={'Tooltip'} data-tip='React-tooltip'> ◕‿‿◕ </a>
-            </UpTooltip>{' '}
-            sur du texte.
+        <>
+            <UpPanel type={'primary'}>
+                Tooltip with top position
+                <UpTooltip
+                    title={'Details'}
+                    content={TooltipRenderer}
+                    place={'top'}
+                >
+                    <UpLigne className={UpLigneStyleContent} >
+                        UpTooltip
+                    </UpLigne>
+                </UpTooltip>
+            </UpPanel>
+            <UpPanel type={'primary'}>
+                Tooltip with bottom position
+                <UpTooltip
+                    title={'Details'}
+                    content={TooltipRenderer}
+                    place={'bottom'}
+                >
+                    <UpLigne className={UpLigneStyleContent} >
+                        UpTooltip
+                    </UpLigne>
+                </UpTooltip>
+            </UpPanel>
+            <UpPanel type={'primary'}>
+                Tooltip with left position
+                <UpTooltip
+                    title={'Details'}
+                    content={TooltipRenderer}
+                    place={'left'}
+                >
+                    <UpLigne className={UpLigneStyleContent} >
+                        UpTooltip
+                    </UpLigne>
+                </UpTooltip>
+            </UpPanel>
+            <UpPanel type={'primary'}>
+                Tooltip with right position
+                <UpTooltip
+                    title={'Details'}
+                    content={TooltipRenderer}
+                    place={'right'}
+                >
+                    <UpLigne className={UpLigneStyleContent} >
+                        UpTooltip
+                    </UpLigne>
+                </UpTooltip>
+            </UpPanel>
+        </>
+    );
+
+PlaceProperty.storyName = 'Change Position By Modifying Place Property';
+
+export const EffectProperty =
+    () => (
+        <UpPanel type={'primary'}>
+            Fixed Tooltip that does not follow mouse cursor
+            <UpTooltip
+                title={'Details'}
+                content={TooltipRenderer}
+                effect={'solid'}
+            >
+                <UpLigne className={UpLigneStyleContent} >
+                    UpTooltip
+                </UpLigne>
+            </UpTooltip>
         </UpPanel>
     );
 
-export const withFormatedText =
+EffectProperty.storyName = 'Display Fix Tooltip';
+
+export const WithCSSBreakLines =
     () => {
         const TooltipContent = 'Text that ends with a new line.\n Text that ends with a new line.\n Text that ends with a new line';
 
         return (
-            <UpTooltip title={'Détails'} content={TooltipContent}>
-                Example of use of the Up Tooltip component
+            <UpTooltip
+                title={'Details'}
+                content={TooltipContent}
+            >
+                Example of use of the UpTooltip component on Icon
                 <UpSvgIcon
                     width={25}
                     height={25}
@@ -60,3 +231,108 @@ export const withFormatedText =
             </UpTooltip>
         )
     };
+
+WithCSSBreakLines.storyName = 'Lines Break By Adding \\n Between Sentences ';
+
+export const MultilinePropertyLinesBreak =
+    () => {
+        const TooltipContent = (
+            <UpParagraph
+                margin={'none'}
+                paragraphSize={'small'}
+            >
+                Text that ends with a new line.<br /> Text that ends with a new line.<br /> Text that ends with a new line
+            </UpParagraph>
+        );
+
+        return (
+            <UpTooltip
+                title={'Details'}
+                content={TooltipContent}
+                multiline={true}
+            >
+                Example of use of the UpTooltip component on button {' '}
+                <UpButton
+                    intent={'primary'}
+                >
+                    Button
+                </UpButton>
+            </UpTooltip>
+        )
+    };
+
+MultilinePropertyLinesBreak.storyName = 'Lines Break By Passing Truthy Multiline Property';
+
+export const DisableTooltip =
+    () => (
+        <UpPanel type={'primary'}>
+            Disable
+            <UpTooltip
+                title={'Details'}
+                content={TooltipRenderer}
+                disable={true}
+            >
+                <UpLigne className={UpLigneStyleContent} >
+                    UpTooltip
+                </UpLigne>
+            </UpTooltip>
+        </UpPanel>
+    );
+
+export const DelayShowHideProperties =
+    () => (
+        <UpPanel type={'primary'}>
+            Show And Hide Tooltip Without Delay
+            <UpTooltip
+                title={'Details'}
+                content={TooltipRenderer}
+                delayHide={0}
+                delayShow={0}
+            >
+                <UpLigne className={UpLigneStyleContent} >
+                    UpTooltip
+                </UpLigne>
+            </UpTooltip>
+        </UpPanel>
+    );
+
+DelayShowHideProperties.storyName = 'Show And Hide Tooltip Without Delay';
+
+export const OnTextAsFunction =
+    () => (
+        <UpPanel type={'primary'}>
+            Example of use of the UpTooltip component on Text
+            <UpTooltip
+                title={'Details'}
+                content={TooltipRenderer}
+            >
+                {
+                    ({ id }) => (
+                        <UpLigne
+                            data-for={id}
+                            data-tip='tooltip'
+                            className={UpLigneStyleContent}
+                        >
+                            UpTooltip
+                        </UpLigne>
+                    )
+                }
+            </UpTooltip>
+        </UpPanel>
+    );
+
+export const OnLink =
+    () => (
+        <UpPanel>
+            Example of use of the UpTooltip component {' '}
+            <UpTooltip
+                id={'Tooltip'}
+                title={'Details'}
+                content={TooltipRenderer}
+            >
+                on Link
+            </UpTooltip>
+        </UpPanel>
+    );
+
+
--- src/Components/Display/Tooltip/types.ts
+++ src/Components/Display/Tooltip/types.ts
@@ -5,18 +5,29 @@ export type Placement = 'top' | 'right' | 'bottom' | 'left';
 export type Effect = 'float' | 'solid';
 
 export interface Tooltip {
+    /** Tooltip content */
     content: JSX.Element | string;
+    /** To specify Tooltip placement */
     place?: Placement;
+    /** To specify Tooltip appearance */
     type?: IntentType;
+    /** To specify behaviour of tooltip */
     effect?: Effect;
+    /** To Display Tooltip text on multiple lines, support \<br>, \<br /> */
     multiline?: boolean;
+    /** To allow HTML in the tooltip */
     html?: boolean;
+    /** Tooltip Title */
     title?: JSX.Element | string;
+    /** Delay hiding the tooltip */
     delayHide?: number;
+    /** Delay showing the tooltip */
     delayShow?: number;
+    /** To remove the ability for an element’s tooltip to be shown */
     disable?: boolean;
 };
 
 export interface UpTooltipProps extends Tooltip {
+    /** To give a unique identifier, otherwise it will be generated automatically */
     id?: string;
 };
