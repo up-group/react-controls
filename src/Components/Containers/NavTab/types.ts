63f77899d63fc6c8a2639fb3233570ebb1741ae0
DESKTOP-5BOU3QA\admin <amine.allouti.ext@up.coop>
2021-03-30 15:01:37 +0200

    Clean types files

--- src/Components/Containers/NavTab/types.ts
+++ src/Components/Containers/NavTab/types.ts
@@ -6,11 +6,11 @@ export interface Tab {
 };
 
 export interface UpNavTabProps {
-    /** Tabs  */
+    /** Tabs */
     tabs: Tab[],
     /** To change the load mode : display tabs in the same or in different containers */
     loadType?: LoadType;
-    /** Callback to execute on tab changing  */
+    /** Callback to execute on tab changing */
     onSelectedTabChanged?: (selectTabKey: number, tab: Tab) => void;
     /** Tab to display when first displaying */
     selectedTabOnLoad?: number
--- src/Components/Containers/Tile/types.ts
+++ src/Components/Containers/Tile/types.ts
@@ -1,6 +1,9 @@
 interface TileProps {
+    /** To provide title */
     title?: string;
+    /** To provide footer */
     footer?: string | JSX.Element;
+    /** To set the maximum height of tile body */
     maxHeight?: string | number;
 };
 
