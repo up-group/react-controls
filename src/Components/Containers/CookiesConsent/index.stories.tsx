85f2c9f435742013b3190343b24262c8d51aad50
Robilol <robin.regis78@gmail.com>
2021-03-30 15:55:22 +0200

    add cookies consent container

---
+++ src/Components/Containers/CookiesConsent/index.stories.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+
+import { UpCookiesConsentProps, UpCookiesConsent } from './UpCookiesConsent';
+
+import { getRootContainer } from '../../../Common/stories';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
+
+export default {
+  title: 'Components/Containers/UpCookiesConsent',
+  decorators : [withKnobs, getRootContainer('UpCookiesConsent')],
+};
+
+export const General =
+  () => (
+    <UpCookiesConsent
+      cookieKey="acceptCookies"
+      cookieValue="authorised"
+      closeButtonLabel="Accepter"
+      cookiesConsentMessage="Des cookies strictement nécessaires à la délivrance du service peuvent être installés lors de votre navigation sur ce site. Nous n'utilisons pas de cookies publicitaires ou à des fins marketing."
+    >
+      <div>
+        <p>Content</p>
+      </div>
+    </UpCookiesConsent>
+  );
+
+export const WithKnowMore =
+  () => (
+    <UpCookiesConsent
+      cookieKey="acceptCookies"
+      cookieValue="authorised"
+      closeButtonLabel="Accepter"
+      cookiesConsentMessage="Des cookies strictement nécessaires à la délivrance du service peuvent être installés lors de votre navigation sur ce site. Nous n'utilisons pas de cookies publicitaires ou à des fins marketing."
+      moreDetailsPage="test"
+      knowMoreLabel="En savoir plus"
+    >
+      <div>
+        <p>Content</p>
+      </div>
+    </UpCookiesConsent>
+  );
