dc11091151cd8ef9d5949b5ffbcd59ea8010f60f
Guillaume CHOMAT <guillaume.chomat@up.coop>
2021-04-15 21:23:23 +0200

    Add nfc icon

--- src/Common/theming/icons.ts
+++ src/Common/theming/icons.ts
@@ -34,7 +34,7 @@ export type IconName =
 | 'file2' | 'home' | 'up-password' | 'condition' | 'on-off' | 'box' | 'box-birth' | 'box-christmas' | 'box-christmas2' 
 | 'box-cross' | 'box-hat' | 'box-heart' | 'box-infinity' | 'box-mustache' | 'box-rings' | 'box-school' | 'box-smile' 
 | 'box-stars' | 'box-trophy' | 'price' | 'letter' | 'post' | 'clear' | 'info' | 'back-arrow' | 'stacking' | 'zendesk' 
-| 'faq' | 'conecs' | 'moveup' | 'eye-open2' | 'file3' | 'invoice' | 'warning';
+| 'faq' | 'conecs' | 'moveup' | 'eye-open2' | 'file3' | 'invoice' | 'warning' | 'nfc';
 
 export const IconNames: IconName[] = ['menu', 'previous', 'list', 'gift', 'attachment', 'add', 'asterisk', 'calendar', 'caret-down',
 'caret-right', 'caret-up', 'close', 'comment', 'confirm', 'cross', 'delete', 'download', 'edit', 'email', 'error-sign', 'export', 
@@ -58,7 +58,7 @@ export const IconNames: IconName[] = ['menu', 'previous', 'list', 'gift', 'attac
 'colored-calendar', 'colored-transaction', 'colored-equalizer', 'colored-card', 'colored-card-inclined', 'colored-list', 'colored-summary', 
 'pen', 'bill-sheet', 'copy', 'duplicate', 'file2', 'home', 'up-password', 'condition', 'on-off', 'box', 'box-birth', 'box-christmas', 'box-christmas2', 
 'box-cross', 'box-hat', 'box-heart', 'box-infinity', 'box-mustache', 'box-rings', 'box-school', 'box-smile', 'box-stars', 'box-trophy', 'price',
-'letter', 'post', 'clear','info','back-arrow','stacking', 'zendesk', 'faq', 'conecs', 'moveup', 'eye-open2', 'file3', 'invoice', 'warning'] ;
+'letter', 'post', 'clear','info','back-arrow','stacking', 'zendesk', 'faq', 'conecs', 'moveup', 'eye-open2', 'file3', 'invoice', 'warning', 'nfc'] ;
 
 let dictionary = {};
 
