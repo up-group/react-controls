a569c399ad106c225839eda952b7abedf378f3d4
Eric LAGRANGE <eric.lagrange.ext@up.coop>
2022-03-02 10:10:57 +0100

    add new confused sweat icon

--- src/Common/theming/mentors.ts
+++ src/Common/theming/mentors.ts
@@ -9,7 +9,8 @@ export type MentorName =
   | 'cry'
   | 'mentor-surprised'
   | 'sorry-orange'
-  | 'confused-grey';
+  | 'confused-grey'
+  | 'confused-with-sweat';
 
 export const MentorNames: MentorName[] = [
   'glasses-orange',
@@ -23,6 +24,7 @@ export const MentorNames: MentorName[] = [
   'mentor-surprised',
   'sorry-orange',
   'confused-grey',
+  'confused-with-sweat',
 ];
 
 const dictionary = {};
