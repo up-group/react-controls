b46d6368edc2a9917062cf632cf55c52fdf239ef
simon.place <simon.place@adevinta.com>
2022-09-08 19:21:42 +0200

    feat(step): Create a component to display step

---
+++ src/Components/Display/ProgressBar/UpProgressBar.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import { style } from 'typestyle';
+import { UpProgressBarTypes, UpStepValues, UpTyleType } from './types';
+
+const Tile = (props: UpTyleType): React.ReactElement => {
+  const { success } = props;
+
+  const tileStyle = style({
+    height: 4,
+    width: 150,
+    justifyContent: 'flex-end',
+    display: 'flex',
+    alignContent: 'center',
+    alignItems: 'center',
+    backgroundColor: success ? '#44cf69' : '#F59100',
+  });
+
+  return (
+      <div className={tileStyle} />
+  );
+};
+
+const Step = (props: UpStepValues): React.ReactElement => {
+  const { value, success, unit } = props;
+
+  const valueWithUnit = `${value}${unit}`;
+  return (
+    <div
+      className={style({
+        backgroundColor: success ? '#22bd4b' : 'lightGrey',
+        width: 60,
+        height: 40,
+        borderRadius: 100,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontWeight: 'bold',
+      })}
+    >
+      {valueWithUnit}
+    </div>
+  );
+};
+
+const UpProgressBar = (props: UpProgressBarTypes): React.ReactElement => {
+  const { values = [], visible = true, unit = '' } = props;
+
+  const ValuesArray = 
+  const containerStyle = style({
+    maxWidth: '300px',
+    width: '100%',
+    display: 'flex',
+    alignContent: 'center',
+    height: 30,
+    alignItems: 'center',
+  });
+
+  return (
+    <div className={containerStyle}>
+      {visible &&
+        values.map(({ step, success }, index) => (
+          <>
+            <Step value={step} key={index} success={success} unit={unit} />
+            {index < values.length - 1 && <Tile success={success} />}
+          </>
+        ))}
+    </div>
+  );
+};
+
+export default UpProgressBar;
