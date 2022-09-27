1de65837437a060e0c5b58b9251700a9111ff309
simon.place <simon.place@adevinta.com>
2022-09-27 15:25:08 +0200

    fix when 0

--- src/Components/Display/ProgressBar/PercentTile.tsx
+++ src/Components/Display/ProgressBar/PercentTile.tsx
@@ -4,7 +4,7 @@ import { percentTyleStyle } from './style';
 const PercentTile = (props: UpPercentTyleType): React.ReactElement => {
   const { success, size } = props;
 
-  return <div className={percentTyleStyle(size, success)} />;
+  return <div className={percentTyleStyle(size === 0 ? 300 : size, success)} />;
 };
 
 export default PercentTile;
