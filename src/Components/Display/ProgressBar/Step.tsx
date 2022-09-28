414161f63a3dd5631329fbdf824c8871c4839a89
simon.place <simon.place@adevinta.com>
2022-09-28 09:15:34 +0200

    upgrade progress bar

--- src/Components/Display/ProgressBar/Step.tsx
+++ src/Components/Display/ProgressBar/Step.tsx
@@ -2,9 +2,10 @@ import { UpStepValues } from './types';
 import { stepStyle } from './style';
 
 const Step = (props: UpStepValues): React.ReactElement => {
-  const { value, success } = props;
+  const { value, success, unit, valueToDisplay } = props;
 
-  return <div className={stepStyle(success)}>{value}</div>;
+  const display = valueToDisplay ? `${valueToDisplay}` : `${value}${unit}`;
+  return <div className={stepStyle(success)}>{display}</div>;
 };
 
 export default Step;
