a5b9e53c94fae3ab17bd663d29ce02aaf40bf971
chomatguillaume <guillaume.chomat@up.coop>
2022-10-25 15:42:41 +0200

    Fix type render value

--- src/Components/Containers/DataGrid/UpDataGrid.tsx
+++ src/Components/Containers/DataGrid/UpDataGrid.tsx
@@ -218,7 +218,7 @@ export interface ToolTip {
   icon?: IconName;
 }
 
-export type RenderValue = { value: any; col: Column };
+export type RenderValue = { value: any; column: Column };
 
 export interface Column {
   label: string | JSX.Element;
