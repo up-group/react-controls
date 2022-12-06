25c38f70da2ab379471099118542922ee54f8e4a
Ludovic Zamord <ludovic.zamord.ext@up.coop>
2022-12-06 18:34:38 +0100

    Adds Collapsable component

---
+++ src/Components/Containers/Collapsable/Collapsable.stories.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Story } from '@storybook/react';
+import { Collapsable, Props } from './Collapsable';
+
+export default {
+  component: Collapsable,
+  title: 'Components/Containers/Collapsable',
+  argTypes: { onClick: { action: 'clicked' } },
+};
+
+const args: Props = {
+  title: 'Informations du commercant',
+  defaultOpened: false,
+  statusIndicator: true,
+};
+
+const Template: Story<Props> = args => (
+  <Collapsable {...args}>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed risus in justo dapibus suscipit.
+      Suspendisse elementum pretium nunc, in dapibus massa pharetra non. Sed id erat consequat, pellentesque felis sed,
+      efficitur nibh. Nullam interdum leo vel augue sodales, mollis consequat lorem faucibus. Quisque commodo erat quis
+      elit ultricies varius. In sit amet risus semper, fringilla nisl non, euismod tortor. Curabitur consequat id sem eu
+      tincidunt.
+    </p>
+  </Collapsable>
+);
+
+export const Default = Template.bind({});
+Default.args = args;
+
+export const WithoutStatusIndicator = Template.bind({});
+WithoutStatusIndicator.args = { ...args, statusIndicator: false };
+
+export const DefaultOpened = Template.bind({});
+DefaultOpened.args = { ...args, defaultOpened: true };
---
+++ src/Components/Containers/Collapsable/Collapsable.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  render,
+  screen,
+  fireEvent,
+  cleanup,
+  RenderResult,
+  waitForElementToBeRemoved,
+  queryByText,
+} from '@testing-library/react';
+
+import { Collapsable, Props } from './Collapsable';
+
+const onClick = jest.fn();
+
+const textContent = 'Lorem ipsum dolor sit amet';
+
+const props: Props = {
+  title: 'Informations du commercant',
+  defaultOpened: false,
+  statusIndicator: true,
+  onClick,
+};
+
+const create = (): RenderResult =>
+  render(
+    <Collapsable {...props}>
+      <p>{textContent}</p>
+    </Collapsable>
+  );
+afterEach(cleanup);
+
+describe('Collapsable', () => {
+  it('should test if component render content correctly', async () => {
+    const { queryByText } = create();
+
+    const title = screen.getByText('Informations du commercant');
+
+    fireEvent.click(title);
+    expect(screen.getByText(textContent)).toBeDefined();
+
+    fireEvent.click(title);
+
+    expect(queryByText(textContent)).toBeNull();
+  });
+});
