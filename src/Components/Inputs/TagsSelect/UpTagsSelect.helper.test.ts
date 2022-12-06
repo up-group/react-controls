28a7f265d7e40f458dbe72162bb60ad0ad47f9d6
Ludovic Zamord <ludovic.zamord.ext@up.coop>
2022-12-06 18:14:46 +0100

    Updates TagsSelect with default selection

--- src/Components/Inputs/TagsSelect/UpTagsSelect.helper.test.ts
+++ src/Components/Inputs/TagsSelect/UpTagsSelect.helper.test.ts
@@ -1,58 +1,58 @@
-import { findAndUpdateSelectedTag, initSelectedTags } from './UpTagsSelect.helper'
+import { findAndUpdateSelectedTag } from './UpTagsSelect.helper';
 
 const tags = [
-    { id: '1', text: 'Mono' },
-    { id: '2', text: 'Multi'},
-    { id: '3', text: 'Tag 3' },
+  { id: '1', text: 'Mono', selected: false },
+  { id: '2', text: 'Multi', selected: false },
+  { id: '3', text: 'Tag 3', selected: false },
 ];
 
 describe('UpTagsSelect.helper', () => {
-    describe('#findAndUpdateSelectedTag', () => {
-        it('should update to true the found selected tag', () => {
-            const selectedTag = {
-                id: '2',
-                text: 'Multi',
-                selected: true,
-            }
-            const result = findAndUpdateSelectedTag(initSelectedTags(tags), selectedTag);
-            const expected = [
-                { id: '1', text: 'Mono' },
-                { id: '2', text: 'Multi', selected: true },
-                { id: '3', text: 'Tag 3' },
-            ];
+  describe('#findAndUpdateSelectedTag', () => {
+    it('should update to true the found selected tag', () => {
+      const selectedTag = {
+        id: '2',
+        text: 'Multi',
+        selected: true,
+      };
+      const result = findAndUpdateSelectedTag(tags, selectedTag);
+      const expected = [
+        { id: '1', text: 'Mono', selected: false },
+        { id: '2', text: 'Multi', selected: true },
+        { id: '3', text: 'Tag 3', selected: false },
+      ];
 
-            expect(result).toMatchObject(expected);
-        });
-        it('should update to false the found selected tag', () => {
-            const selectedTag = {
-                id: '2',
-                text: 'Multi',
-                selected: false,
-            }
-            const result = findAndUpdateSelectedTag(initSelectedTags(tags), selectedTag);
-            const expected = [
-                { id: '1', text: 'Mono' },
-                { id: '2', text: 'Multi', selected: false },
-                { id: '3', text: 'Tag 3' },
-            ];
-
-            expect(result).toMatchObject(expected);
-        });
-
-        it('should not update the tags array if the tag is not found', () => {
-            const selectedTag = {
-                id: '4',
-                text: 'Multi',
-                selected: false,
-            }
-            const result = findAndUpdateSelectedTag(initSelectedTags(tags), selectedTag);
-            const expected = [
-                { id: '1', text: 'Mono' },
-                { id: '2', text: 'Multi'},
-                { id: '3', text: 'Tag 3' },
-            ];
-
-            expect(result).toMatchObject(expected);
-        });
+      expect(result).toMatchObject(expected);
     });
+    it('should update to false the found selected tag', () => {
+      const selectedTag = {
+        id: '2',
+        text: 'Multi',
+        selected: false,
+      };
+      const result = findAndUpdateSelectedTag(tags, selectedTag);
+      const expected = [
+        { id: '1', text: 'Mono', selected: false },
+        { id: '2', text: 'Multi', selected: false },
+        { id: '3', text: 'Tag 3', selected: false },
+      ];
+
+      expect(result).toMatchObject(expected);
+    });
+
+    it('should not update the tags array if the tag is not found', () => {
+      const selectedTag = {
+        id: '4',
+        text: 'Multi',
+        selected: false,
+      };
+      const result = findAndUpdateSelectedTag(tags, selectedTag);
+      const expected = [
+        { id: '1', text: 'Mono', selected: false },
+        { id: '2', text: 'Multi', selected: false },
+        { id: '3', text: 'Tag 3', selected: false },
+      ];
+
+      expect(result).toMatchObject(expected);
+    });
+  });
 });
--- src/Components/Inputs/TagsSelect/UpTagsSelect.stories.tsx
+++ src/Components/Inputs/TagsSelect/UpTagsSelect.stories.tsx
@@ -1,36 +1,46 @@
-import React from "react";
-import { Story } from "@storybook/react";
-import { UpTagsSelect, Props } from "./UpTagsSelect";
+import React from 'react';
+import { Story } from '@storybook/react';
+import { UpTagsSelect, Props } from './UpTagsSelect';
 
 export default {
-    component: UpTagsSelect,
-    title: 'Components/Inputs/UpTagsSelect',
-    argTypes: { onChange: { action: 'selected' } },
+  component: UpTagsSelect,
+  title: 'Components/Inputs/UpTagsSelect',
+  argTypes: { onChange: { action: 'selected' } },
 };
 
 const tags = [
-    {
-        id: '1',
-        text: 'Mono'
-    },
-    {
-        id: '2',
-        text: 'Multi'
-    },
-    {
-        id: '3',
-        text: 'Tag 3'
-    },
+  {
+    id: '1',
+    text: 'Mono',
+  },
+  {
+    id: '2',
+    text: 'Multi',
+  },
+  {
+    id: '3',
+    text: 'Tag 3',
+  },
 ];
 
 const args = {
-    tags
-}
+  tags,
+};
 
-const Template: Story<Props> = (args) => <UpTagsSelect {...args} />;
+const Template: Story<Props> = args => <UpTagsSelect {...args} />;
+
+export const WithLabel = Template.bind({});
+WithLabel.args = { ...args, label: "Nombre d'anomalies par titre" };
 
 export const Default = Template.bind({});
 Default.args = args;
 
-export const WithLabel = Template.bind({});
-WithLabel.args = {...args, label: 'Nombre d\'anomalies par titre'};
+export const WithDefaultSelected = Template.bind({});
+WithDefaultSelected.args = {
+  ...args,
+  tags: [
+    { id: '1', text: 'Mono', selected: true },
+    { id: '2', text: 'Multi', selected: true },
+    { id: '3', text: 'Tag 3', selected: false },
+  ],
+};
