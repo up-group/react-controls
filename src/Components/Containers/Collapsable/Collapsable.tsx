5804cdee42433ca23830030ef5284dc0d6a77d73
Ludovic Zamord <ludovic.zamord.ext@up.coop>
2022-12-08 13:40:59 +0100

    :refactor: Moves useTheme to the common hooks folder

--- src/Components/Containers/Collapsable/Collapsable.tsx
+++ src/Components/Containers/Collapsable/Collapsable.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useTheme } from '../../../Common/theming/ThemeContext';
+import { useTheme } from '../../../Common/hooks/useTheme';
 import {
   getWrapperStyles,
   getTitleStyles,
