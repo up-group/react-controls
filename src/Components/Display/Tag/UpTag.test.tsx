cdf05314f135a6d02fe00d575330f942b75f503c
Ludovic Zamord <ludovic.zamord.ext@up.coop>
2022-12-14 14:59:14 +0100

    Prettify UpTag testfile

--- src/Components/Display/Tag/UpTag.test.tsx
+++ src/Components/Display/Tag/UpTag.test.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, RenderResult, screen } from '@testing-library/react';
 import { UpTag, Props } from './UpTag';
 
 const onChange = jest.fn();
 
-let props: Props = {
-    id: '1',
-    text: 'Mono',
-    onChange
+const props: Props = {
+  id: '1',
+  text: 'Mono',
+  onChange,
+  selected: false,
 };
 
-const create = () => render(<UpTag {...props} />);
+const create = (): RenderResult => render(<UpTag {...props} />);
 
-describe("Tag", () => {
-    it("should test if component render content correctly", () => {
-        create();
+describe('UpTag', () => {
+  it('should test if component render content correctly', () => {
+    create();
 
-        const text = screen.getByText('Mono');
-        const id = screen.getByTestId(`tag-${props.id}`);
+    const text = screen.getByText('Mono');
+    const id = screen.getByTestId(`tag-${props.id}`);
 
-        expect(text).toBeDefined();
-        expect(id).toBeDefined();
-    });
+    expect(text).toBeDefined();
+    expect(id).toBeDefined();
+  });
 });
